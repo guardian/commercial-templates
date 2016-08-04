91099a0deb2f32b5efe0d493447736e7cc98b1dd
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-08-04 15:33:35 +0100

    Complete work on Glabs NTD

--- src/glabs-native-traffic-driver/index.js
+++ src/glabs-native-traffic-driver/index.js
@@ -1,3 +1,5 @@
 import { getWebfonts } from '../_shared/js/messages.js';
+import { enableToggles } from '../_shared/js/ui.js';
 
 getWebfonts(['GuardianSansWeb', 'GuardianTextSansWeb']);
+enableToggles();
