150789084fdadbce45eeffa326adef0bd8517474
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-09-13 12:49:39 +0100

    Remove unwanted files

--- src/_shared/js/colls.js
+++
@@ -1,9 +0,0 @@
-export function range({ start = 0, end = 100, step = 1 }) {
-    let coll = [];
-    let val = start;
-    while(val < end) {
-        coll.push(val);
-        val += step;
-    }
-    return coll;
-}
