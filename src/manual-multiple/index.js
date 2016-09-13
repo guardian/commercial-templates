619a23d7c5e63d7dcbb50b7c77710f34b14e809d
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-09-13 17:39:38 +0100

    Add manual multiple soulmates flavour

--- src/manual-multiple/index.js
+++ src/manual-multiple/index.js
@@ -1,4 +1,7 @@
 import { getIframeId, getWebfonts } from '../_shared/js/messages.js';
+import { cleanupButtons } from '../_shared/js/creatives.js';
+
+cleanupButtons();
 
 getIframeId().then(() => {
     getWebfonts();
