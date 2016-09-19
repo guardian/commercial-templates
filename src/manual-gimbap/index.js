181e3a162e35474ad064e4fa8ac3b301d1a2503b
Jamie Byers <james.byers@guardian.co.uk>
2016-09-19 11:36:50 +0100

    Migrated gimbap logos.

--- src/manual-gimbap/index.js
+++ src/manual-gimbap/index.js
@@ -1,3 +1,9 @@
 Array.from(document.getElementsByClassName('gimbap__title'), (title) => {
-	title.insertAdjacentHtml('afterbegin', quoteSvg)
+	title.insertAdjacentHtml('afterbegin', quoteSvg);
+});
+
+var offerTones = ['[%Offer1Tone%]', '[%Offer2Tone%]', '[%Offer3Tone%]', '[%Offer4Tone%]'];
+
+Array.from(document.getElementsByClassName('gimbap-logo'), (logo, idx) => {
+	logo.insertAdjacentHtml('afterbegin', logoSvgs[offerTones[idx]]);
 });
