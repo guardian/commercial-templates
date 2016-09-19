f1c10d598004d1b1c5e43c0f5b5dfa536a863eb1
Jamie Byers <james.byers@guardian.co.uk>
2016-09-19 16:37:57 +0100

    Fixed styling issues with gimbap.

--- src/manual-gimbap/index.js
+++ src/manual-gimbap/index.js
@@ -1,3 +1,10 @@
+// Webfonts
+import { getIframeId, getWebfonts } from '../_shared/js/messages.js';
+
+getIframeId().then(() => {
+    getWebfonts();
+});
+
 // Adds quotes to title if set in DFP.
 Array.from(document.getElementsByClassName('gimbap__title'), (title) => {
 	title.insertAdjacentHTML('afterbegin', quoteSvg);
@@ -12,12 +19,21 @@ Array.from(document.getElementsByClassName('gimbap-logo'), (logo, idx) => {
 });
 
 // Adds author image if there is an author image URL present in DFP.
-if ('[%Offer1AuthorImage%]') {
+if (authorImage !== "") {
 
 	var firstGimbap = document.getElementsByClassName('gimbap')[0];
 
 	firstGimbap.classList.add('gimbap--has-author');
 	firstGimbap.insertAdjacentHTML('afterbegin',
-		'<img class="gimbap__author" src="[%Offer1AuthorImage%]" alt="" />');
+		`<img class="gimbap__author" src="${authorImage}" alt="" />`);
+
+}
+
+// Displays arrows if set in DFP.
+if (arrowLinks === 'yes') {
+
+	Array.from(document.getElementsByClassName('gimbap__arrow'), (arrow) => {
+		arrow.classList.add('gimbap__arrow--show')
+	});
 
 }
