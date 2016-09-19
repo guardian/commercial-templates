09c2572d18a7233e7e91a508fb0e612d424f8935
jon_norman <jon.norman@theguardian.com>
2016-09-19 11:32:08 +0100

    Add in AJAX call to Soulmates API for JSON payload.
    The actual card rendering is also refactored for clarity.

--- src/merchandising-soulmates/index.js
+++ src/merchandising-soulmates/index.js
@@ -1,26 +1,34 @@
+import { ajaxCall } from './ajax';
+
+requestSoulmates([%Subfeed%])
+  .catch( (error) => {throw new AJAXError(error); })
+  .then(JSON.parse)
+  .catch( (error) => {throw new JSONError(error); })
+  .then( (parsedSoulmates) => addSoulmateCards(parsedSoulmates));
+
+function requestSoulmates(group) {
+  ajaxCall('https://theguardian.com/commercial/soulmates.json',
+           'group=' + group)
+});
+
 function addSoulmateCards(soulmates) {
-
-  /*
-  We want to create the following structure for each soulmate card:
-
-  <a class="advert advert--soulmate" href="%%CLICK_URL_ESC%%{{profileUrl}}" data-link-name="merchandising-soulmates-v2_2_2014-03-28-profile-@{soulmate.gender}">
-    <h2 class="advert__title u-text-hyphenate" itemprop="name">{{username}}</h2>
-    <div class="advert__image-container">
-      <img class="advert__image" src="{{profilePhoto}}">
-    </div>
-    <div class="advert__meta">{{age}}, {{location}}</div>
-  </a>
-  */
-
   /*
     We can use the nice <template> tag if it is supported:
     https://developer.mozilla.org/en/docs/Web/HTML/Element/template
+
+    otherwise create it incrementally.
   */
+  if (supportsTemplate) {
+    generateWithTemplate();
+  } else {
+    generateWithoutTemplate();
+  }
+
   function supportsTemplate (){
     return 'content' in document.createElement('template')
   }
 
-  if (supportsTemplate) {
+  function generateWithTemplate() {
     let template = document.querySelector("#soulmate-card");
     let card_container = document.getElementByClassName("adverts_row");
 
@@ -38,8 +46,9 @@ function addSoulmateCards(soulmates) {
       // copy the template into the card container
       var clone = document.importNode(t.content, true);
       card_container.appendChild(clone);
-    }
-  } else {
+  }
+
+  function generateWithoutTemplate() {
     for (soulmate of soulmates) {
 
       // create the elements separately
