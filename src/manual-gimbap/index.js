49fe1d443d5abd6d7c69eb69d3b56ca8f3ee7258
Jamie Byers <james.byers@guardian.co.uk>
2016-09-19 11:56:10 +0100

    Insertion of author class to first gimbap now occurs in js.

--- src/manual-gimbap/index.js
+++ src/manual-gimbap/index.js
@@ -1,9 +1,17 @@
+// Adds quotes to title if set in DFP.
 Array.from(document.getElementsByClassName('gimbap__title'), (title) => {
 	title.insertAdjacentHtml('afterbegin', quoteSvg);
 });
 
-var offerTones = ['[%Offer1Tone%]', '[%Offer2Tone%]', '[%Offer3Tone%]', '[%Offer4Tone%]'];
+// Inserts appropriate SVG logos for certain tones.
+var offerTones = ['[%Offer1Tone%]', '[%Offer2Tone%]', '[%Offer3Tone%]',
+	'[%Offer4Tone%]'];
 
 Array.from(document.getElementsByClassName('gimbap-logo'), (logo, idx) => {
 	logo.insertAdjacentHtml('afterbegin', logoSvgs[offerTones[idx]]);
 });
+
+// Adds author class if there is an author image URL present in DFP.
+if ('[%Offer1AuthorImage%]') {
+	document.getElementsByClassName('gimbap')[0].classList.add('gimbap--has-author');
+}
