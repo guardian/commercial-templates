5d835d1ce0801bc2b361a08bea4b21ffecf1181b
Jamie Byers <james.byers@guardian.co.uk>
2016-09-19 12:12:35 +0100

    Minor refactor and js bugfixes.

--- src/manual-gimbap/index.js
+++ src/manual-gimbap/index.js
@@ -1,6 +1,6 @@
 // Adds quotes to title if set in DFP.
 Array.from(document.getElementsByClassName('gimbap__title'), (title) => {
-	title.insertAdjacentHtml('afterbegin', quoteSvg);
+	title.insertAdjacentHTML('afterbegin', quoteSvg);
 });
 
 // Inserts appropriate SVG logos for certain tones.
@@ -8,7 +8,7 @@ var offerTones = ['[%Offer1Tone%]', '[%Offer2Tone%]', '[%Offer3Tone%]',
 	'[%Offer4Tone%]'];
 
 Array.from(document.getElementsByClassName('gimbap-logo'), (logo, idx) => {
-	logo.insertAdjacentHtml('afterbegin', logoSvgs[offerTones[idx]]);
+	logo.insertAdjacentHTML('afterbegin', logoSvgs[offerTones[idx]]);
 });
 
 // Adds author image if there is an author image URL present in DFP.
@@ -17,7 +17,7 @@ if ('[%Offer1AuthorImage%]') {
 	var firstGimbap = document.getElementsByClassName('gimbap')[0];
 
 	firstGimbap.classList.add('gimbap--has-author');
-	firstGimbap.insertAdjacentHtml('afterbegin',
+	firstGimbap.insertAdjacentHTML('afterbegin',
 		'<img class="gimbap__author" src="[%Offer1AuthorImage%]" alt="" />');
 
 }
