a730c4bf9cbb6a63a5e91d733a13112c728fd79b
Jamie Byers <james.byers@guardian.co.uk>
2016-09-21 10:43:58 +0100

    Wrapped gimbap DOM manipulations in write call, to make use of requestAnimationFrame.

--- src/manual-gimbap/index.js
+++ src/manual-gimbap/index.js
@@ -1,50 +1,60 @@
 // Webfonts
 import { getIframeId, getWebfonts } from '../_shared/js/messages.js';
+import { write } from '../_shared/js/dom.js';
 
 getIframeId().then(() => {
     getWebfonts();
 });
 
-// Adds quotes to title if set in DFP.
+// Retrieve DFP variables.
 var offerQuotes = ['[%Offer1Quotes%]', '[%Offer2Quotes%]', '[%Offer3Quotes%]',
 	'[%Offer4Quotes%]'];
-
-Array.from(document.getElementsByClassName('gimbap__title'), (title, idx) => {
-
-	if (offerQuotes[idx] === 'yes') {
-		title.insertAdjacentHTML('afterbegin', quoteSvg);
-	}
-
-});
-
-// Inserts appropriate SVG logos for certain tones.
 var offerTones = ['[%Offer1Tone%]', '[%Offer2Tone%]', '[%Offer3Tone%]',
 	'[%Offer4Tone%]'];
-
-Array.from(document.getElementsByClassName('gimbap-logo'), (logo, idx) => {
-	logo.insertAdjacentHTML('afterbegin', logoSvgs[offerTones[idx]]);
-});
-
 var authorImage = '[%Offer1AuthorImage%]';
-
-// Adds author image if there is an author image URL present in DFP.
-if (authorImage !== "") {
-
-	var firstGimbap = document.getElementsByClassName('gimbap')[0];
-
-	firstGimbap.classList.add('gimbap--has-author');
-	firstGimbap.insertAdjacentHTML('afterbegin',
-		`<img class="gimbap__author" src="${authorImage}" alt="" />`);
-
-}
-
-// Displays arrows if set in DFP.
 var arrowLinks = '[%LinksWithArrows%]';
 
-if (arrowLinks === 'yes') {
+// DOM mutations.
+write(() => {
 
-	Array.from(document.getElementsByClassName('gimbap__arrow'), (arrow) => {
-		arrow.classList.add('gimbap__arrow--show')
+	// Adds quotes to title if set in DFP.
+	Array.from(document.getElementsByClassName('gimbap__title'), insertQuotes);
+
+	function insertQuotes (title, adNumber) {
+
+		if (offerQuotes[adNumber] === 'yes') {
+			title.insertAdjacentHTML('afterbegin', quoteSvg);
+		}
+
+	}
+
+	// Inserts appropriate SVG logos for certain tones.
+	Array.from(document.getElementsByClassName('gimbap-logo'), (logo, idx) => {
+		logo.insertAdjacentHTML('afterbegin', logoSvgs[offerTones[idx]]);
 	});
 
-}
+	// Adds author image if there is an author image URL present in DFP.
+	if (authorImage !== "") {
+
+		var firstGimbap = document.getElementsByClassName('gimbap')[0];
+
+		firstGimbap.classList.add('gimbap--has-author');
+		firstGimbap.insertAdjacentHTML('afterbegin',
+			`<img class="gimbap__author" src="${authorImage}" alt="" />`);
+
+	}
+
+	// Displays arrows if set in DFP.
+	if (arrowLinks === 'yes') {
+
+		Array.from(document.getElementsByClassName('gimbap__arrow'), showArrow);
+
+		function showArrow (arrow) {
+			arrow.classList.add('gimbap__arrow--show');
+		}
+
+	}
+
+}).catch(err => {
+	console.log(err);
+});
