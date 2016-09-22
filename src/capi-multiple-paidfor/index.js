0d95ee74ffb858acbe7b4f1506089a2ea2875f14
Jamie Byers <james.byers@guardian.co.uk>
2016-09-22 11:55:40 +0100

    Added card template to capi-multiple-paidfor.

---
+++ src/capi-multiple-paidfor/index.js
@@ -0,0 +1,10 @@
+// Webfonts
+import { getIframeId, getWebfonts } from '../_shared/js/messages.js';
+import { write } from '../_shared/js/dom.js';
+
+// DOM mutations.
+write(() => {
+
+}).catch(err => {
+	console.log(err);
+});
