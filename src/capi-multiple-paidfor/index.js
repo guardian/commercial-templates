94f218f88fff6111a215d82886b1795941e3e01d
Jamie Byers <james.byers@guardian.co.uk>
2016-09-22 12:19:26 +0100

    Set up js skeleton for creating and inserting cards.

--- src/capi-multiple-paidfor/index.js
+++ src/capi-multiple-paidfor/index.js
@@ -2,9 +2,44 @@
 import { getIframeId, getWebfonts } from '../_shared/js/messages.js';
 import { write } from '../_shared/js/dom.js';
 
-// DOM mutations.
-write(() => {
+// Loads the card data from CAPI in JSON format.
+function retrieveCapiData () {
 
-}).catch(err => {
+	// Do request stuff.
+
+}
+
+// Constructs an individual card.
+function buildCard (cardInfo) {
+
+	let cardTemplate = document.getElementById('paidfor-card');
+	let card = document.importNode(cardTemplate.card, true);
+
+	return card;
+
+}
+
+// Constructs an array of cards from an array of data.
+function buildCards (cardsInfo) {
+
+	let cardList = document.createDocumentFragment();
+
+	cardsInfo.foreach(info => {
+		cardList.appendChild(buildCard(info));
+	});
+
+	return () => {
+
+		let advertRow = document.getElementsByClassName('adverts__row')[0];
+		advertRow.appendChild(cardList);
+
+	};
+
+}
+
+// Inserts cards into the advert, retrieving their data from CAPI.
+retrieveCapiData().then((data) => {
+	return buildCards(data);
+}).then(write).catch(err => {
 	console.log(err);
 });
