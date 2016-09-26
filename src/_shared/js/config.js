d94607ee5b039fe5eb95c2ad44ca7e8493a6b026
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-09-26 15:51:18 +0100

    Add jobs component

--- src/_shared/js/config.js
+++ src/_shared/js/config.js
@@ -1,4 +1,5 @@
 export default {
     travelUrl: '/commercial/travel/api/offers.json',
-    booksUrl: '/commercial/books/api/books.json'
+    booksUrl: '/commercial/books/api/books.json',
+    jobsUrl: '/commercial/api/jobs.json'
 }
