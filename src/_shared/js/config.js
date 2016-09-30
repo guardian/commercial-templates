56df768858562ac43ec598aa2d68e87d7c322720
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-09-30 12:20:51 +0100

    Add blended URL

--- src/_shared/js/config.js
+++ src/_shared/js/config.js
@@ -2,5 +2,6 @@ export default {
     travelUrl: '/commercial/travel/api/offers.json',
     booksUrl: '/commercial/books/api/books.json',
     jobsUrl: '/commercial/jobs/api/jobs.json',
+    blendedUrl: '/commercial/api/multi.json',
     soulmatesUrl: '/commercial/soulmates/api/[%SubFeed%].json'
 }
