e53b4654b046a4e05ef0a6f58b79e4612d4b9b69
Jamie Byers <james.byers@guardian.co.uk>
2016-10-06 17:36:49 +0100

    Set up edition link and wired up build functions into async chain.

--- src/glabs-native-traffic-driver/index.js
+++ src/glabs-native-traffic-driver/index.js
@@ -1,19 +1,30 @@
-import { getIframeId, getWebfonts, resizeIframeHeight } from '../_shared/js/messages.js';
+import { getIframeId, getWebfonts, resizeIframeHeight } from
+	'../_shared/js/messages.js';
 import { enableToggles } from '../_shared/js/ui.js';
 import { addTrackingPixel } from '../_shared/js/ads.js';
+import { write } from '../_shared/js/dom.js';
+import { getApiBaseUrl } from '../_shared/js/dev';
+
+// Glabs edition links.
+const GLABS_EDITION = {
+	default: 'https://theguardian.com/guardian-labs',
+	au: 'https://theguardian.com/guardian-labs-australia',
+	us: 'https://theguardian.com/guardian-labs-us'
+};
 
 const OVERRIDES = {
-	headline: '[%ArticleHeaderText%]',
-	text: '[%ArticleText%]'
+	headline: "[%ArticleHeaderText%]",
+	text: "[%ArticleText%]"
 };
 
 // Loads the article data from CAPI in JSON format.
-function retrieveCapiData () {
+function retrieveCapiData ({host, preview}) {
 
+	let endpoint = `${getApiBaseUrl(host, preview)}/commercial/jobs/api/jobs.json?`;
 	let params = new URLSearchParams();
 	params.append('t', '[%ArticleShortURL%]')
 
-	let url = `${ENDPOINT}?${params}`;
+	let url = `${endpoint}?${params}`;
 
 	return fetch(url).then(response => response.json());
 
@@ -29,6 +40,21 @@ function insertText (capiText, containerClass, override) {
 
 }
 
+// Sets correct glabs link based on edition (AU/All others).
+function editionLink (edition) {
+
+	let link = GLABS_EDITION.default;
+
+	if (edition === 'AU') {
+		link = GLABS_EDITION.au;
+	} else if (edition === 'US') {
+		link = GLABS_EDITION.us;
+	}
+
+	document.querySelector('.creative__glabs-link').href = link;
+
+}
+
 // Uses cAPI data to build the ad content.
 function buildFromCapi (data) {
 
@@ -36,16 +62,23 @@ function buildFromCapi (data) {
 
 		insertText(data.articleHeadline, 'creative__title', OVERRIDES.headline);
 		insertText(data.articleText, 'creative__standfirst', OVERRIDES.text);
-		document.querySelector('.creative__ctu') = data.articleUrl;
+		document.querySelector('.creative__ctu').href = data.articleUrl;
+		editionLink(data.edition);
 
 	};
 
 }
 
 getIframeId()
+.then(retrieveCapiData)
+.then(buildFromCapi)
+.then(write)
 .then(() => {
     enableToggles();
     addTrackingPixel(document.getElementById('creative'));
-    return getWebfonts(['GuardianSansWeb', 'GuardianTextSansWeb'])
 })
-.then(resizeIframeHeight);
+.then(getWebfonts)
+.then(resizeIframeHeight)
+.catch(err => {
+	console.log(err);
+});
