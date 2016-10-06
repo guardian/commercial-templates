2fd9b906b094374721ae371e0df12936a7ef2743
Calum Campbell <campbell.cj@hotmail.co.uk>
2016-10-06 16:31:35 +0100

    add effect class

--- src/manual-gimbap-simple/index.js
+++ src/manual-gimbap-simple/index.js
@@ -9,10 +9,13 @@ function formatGimbap() {
 
     let linksWithArrows = '[%linksWithArrows%]'
     let componentTone   = '[%componenttone%]'
+    let gimbapEffects   = '[%gimbapEffects%]'
 
     Array.from(document.getElementsByClassName('gimbap-logo')).forEach(insertHeaderSvg);
 
     if (linksWithArrows === 'yes') Array.from(document.getElementsByClassName('gimbap__cta--simple')).forEach(showArrow);
+
+    if (gimbapEffects === 'yes') Array.from(document.getElementsByClassName('gimbap--simple')).forEach(addEffectClass);
 
     function showArrow (div) {
         div.insertAdjacentHTML('beforeend', arrowRightSvg)
@@ -20,5 +23,9 @@ function formatGimbap() {
 
     function insertHeaderSvg(div) {
         div.insertAdjacentHTML('afterbegin', logoSvgs[componentTone]);
+    }
+
+    function addEffectClass(div) {
+        div.className += " gimbap--effects";
     }
 };
