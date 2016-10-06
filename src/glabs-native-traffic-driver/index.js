8c37f7a40a4ad55b7a75e8c079c7970535aef3cb
Jamie Byers <james.byers@guardian.co.uk>
2016-10-06 15:04:20 +0100

    Added function to generate a headline for traffic driver.

--- src/glabs-native-traffic-driver/index.js
+++ src/glabs-native-traffic-driver/index.js
@@ -2,6 +2,25 @@ import { getIframeId, getWebfonts, resizeIframeHeight } from '../_shared/js/mess
 import { enableToggles } from '../_shared/js/ui.js';
 import { addTrackingPixel } from '../_shared/js/ads.js';
 
+const OVERRIDES = {
+	headline: '[%ArticleHeaderText%]'
+};
+
+// Inserts a
+function insertHeadline (articleInfo) {
+
+	let headline;
+
+	if (OVERRIDES.headline !== '') {
+		headline = OVERRIDES.headline;
+	} else {
+		headline = articleInfo.articleHeadline;
+	}
+
+	document.querySelector('.creative__title').textContent = headline;
+
+}
+
 getIframeId()
 .then(() => {
     enableToggles();
