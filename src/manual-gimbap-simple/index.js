cc3c142c407cbf91b2e2f4c2308fb9df18ddcaf9
Calum Campbell <campbell.cj@hotmail.co.uk>
2016-10-12 11:45:46 +0100

    update arrows functionality to use css not JS

--- src/manual-gimbap-simple/index.js
+++ src/manual-gimbap-simple/index.js
@@ -7,19 +7,19 @@ getIframeId()
 
 function formatGimbap() {
 
-    let linksWithArrows = '[%LinksWithArrows%]';
+    // let linksWithArrows = '[%LinksWithArrows%]';
     let componentTone   = '[%ComponentTone%]';
     let gimbapEffects   = '[%GimbapEffects%]';
 
     Array.from(document.getElementsByClassName('gimbap-logo')).forEach(insertHeaderSvg);
 
-    if (linksWithArrows === 'yes') Array.from(document.getElementsByClassName('gimbap__cta--simple')).forEach(showArrow);
+    // if (linksWithArrows === 'yes') Array.from(document.getElementsByClassName('gimbap__cta--simple')).forEach(showArrow);
 
     if (gimbapEffects === 'yes') Array.from(document.getElementsByClassName('gimbap--simple')).forEach(addEffectClass);
 
-    function showArrow (div) {
-        div.insertAdjacentHTML('beforeend', arrowRightSvg)
-    }
+    // function showArrow (div) {
+    //     div.insertAdjacentHTML('beforeend', arrowRightSvg)
+    // }
 
     function insertHeaderSvg(div) {
         div.insertAdjacentHTML('afterbegin', logoSvgs[componentTone]);
