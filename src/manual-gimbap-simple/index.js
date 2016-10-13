24929012f3ac0cd7bab52312a9f2d8669700e95e
Calum Campbell <campbell.cj@hotmail.co.uk>
2016-10-13 11:06:03 +0100

    add gimbap simple click reporting

--- src/manual-gimbap-simple/index.js
+++ src/manual-gimbap-simple/index.js
@@ -1,25 +1,19 @@
 // Webfonts
-import { getIframeId, getWebfonts, resizeIframeHeight } from '../_shared/js/messages.js';
+import { getIframeId, getWebfonts, resizeIframeHeight, reportClicks } from '../_shared/js/messages.js';
 
 getIframeId()
-    .then(() => Promise.all([getWebfonts(), formatGimbap()]))
+    .then(() => Promise.all([getWebfonts(), reportClicks(), formatGimbap()]))
 .then(resizeIframeHeight);
 
 function formatGimbap() {
 
-    // let linksWithArrows = '[%LinksWithArrows%]';
     let componentTone   = '[%ComponentTone%]';
     let gimbapEffects   = '[%GimbapEffects%]';
 
     Array.from(document.getElementsByClassName('gimbap-logo')).forEach(insertHeaderSvg);
 
-    // if (linksWithArrows === 'yes') Array.from(document.getElementsByClassName('gimbap__cta--simple')).forEach(showArrow);
 
     if (gimbapEffects === 'yes') Array.from(document.getElementsByClassName('gimbap--simple')).forEach(addEffectClass);
-
-    // function showArrow (div) {
-    //     div.insertAdjacentHTML('beforeend', arrowRightSvg)
-    // }
 
     function insertHeaderSvg(div) {
         div.insertAdjacentHTML('afterbegin', logoSvgs[componentTone]);
