5fbf0623579078aad5edfcfb2bd9d42641bf440f
Calum Campbell <campbell.cj@hotmail.co.uk>
2016-10-13 11:13:43 +0100

    update manual inline to include click reporting

--- src/manual-inline/index.js
+++ src/manual-inline/index.js
@@ -1,5 +1,6 @@
-import { getIframeId, resizeIframeHeight,getWebfonts } from '../_shared/js/messages.js';
+import { getIframeId, resizeIframeHeight, reportClicks,getWebfonts } from '../_shared/js/messages.js';
 
 getIframeId()
     .then(() => getWebfonts())
+    .then(() => reportClicks())
     .then(resizeIframeHeight);
