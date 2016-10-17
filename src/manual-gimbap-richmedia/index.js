6d9b9b0a950063e6c37fd1f64db12d3eaf6558bb
Calum Campbell <campbell.cj@hotmail.co.uk>
2016-10-17 09:50:14 +0100

    css and html update

--- src/manual-gimbap-richmedia/index.js
+++ src/manual-gimbap-richmedia/index.js
@@ -9,9 +9,35 @@ function formatGimbap() {
     let componentTone   = '[%ComponentTone%]';
     let gimbapEffects   = '[%GimbapEffects%]';
 
+    [
+        ['date','[%Offer1Date%]'],
+        ['price','[%Offer1Price%]'],
+        ['place', '[%Offer1Place%]'],
+        ['saving', '[%Offer1Discount'],
+        ['date','[%Offer2Date%]'],
+        ['price','[%Offer2Price%]'],
+        ['place', '[%Offer2Place%]'],
+        ['saving', '[%Offer2Discount']
+    ].forEach(checkMeta);
+
     Array.from(document.getElementsByClassName('gimbap-logo')).forEach(insertHeaderSvg);
 
     if (gimbapEffects === 'yes') Array.from(document.getElementsByClassName('gimbap--simple')).forEach(addEffectClass);
+
+    function checkMeta(pair,index,array){
+
+        if(!pair[1].includes('[%')) {
+            if(index < array.length/2){
+                removeHideClass(Array.from(document.getElementsByClassName('gimbap-richmedia__meta--' + pair[0]))[0])
+            }else{
+                removeHideClass(Array.from(document.getElementsByClassName('gimbap-richmedia__meta--' + pair[0]))[1])
+            }
+        }
+    }
+
+    function removeHideClass(div){
+        div.classList.remove('metaHide');
+    }
 
     function insertHeaderSvg(div) {
         div.insertAdjacentHTML('afterbegin', logoSvgs[componentTone]);
