368bb78162af64304ed6f1378bb8eee8549ce5a1
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-10-17 12:00:57 +0100

    Add Fabric Video template

---
+++ src/fabric-video/index.js
@@ -0,0 +1,59 @@
+import { getIframeId, onScroll, onViewport } from '../_shared/js/messages';
+import { write } from '../_shared/js/dom';
+
+getIframeId()
+.then(() => {
+    let video = document.getElementsByTagName('video')[0];
+    let layer2 = Array.from(document.getElementsByClassName('creative__layer2'));
+    let inView = false;
+    let isUpdating = false;
+    let played = false;
+
+    video.onended = () => played = true;
+
+    onViewport(({ height }) => {
+        let viewportHeight = height;
+        onScroll(({ top, bottom }) => {
+            if( played ) {
+                return false;
+            }
+
+            inView = top >= 0 && bottom < viewportHeight;
+            if (!isUpdating) {
+                isUpdating = true;
+                updateVideo();
+                write(updateView);
+            }
+        });
+    });
+
+    function updateView() {
+        isUpdating = false;
+        updateAnimation();
+    }
+
+    function updateVideo() {
+        if (inView) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    }
+
+    function updateAnimation() {
+        if (inView) {
+            playAnimation();
+        } else {
+            pauseAnimation();
+        }
+    }
+
+    function playAnimation() {
+        layer2.forEach(function (l) { l.classList.add('is-animating'); });
+    }
+
+    function pauseAnimation() {
+        layer2.forEach(function (l) { l.classList.remove('is-animating'); });
+    }
+
+});
