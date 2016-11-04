c3dd9710638aaa431cdc624813ac3ae8322443ef
Anne Byrne <anne.byrne@guardian.co.uk>
2016-11-04 12:09:45 +0000

    Moved to using Facebook html snippet

--- src/facebook/index.js
+++ src/facebook/index.js
@@ -8,57 +8,22 @@ getIframeId()
 .then(() => getWebfonts())
 .then(resizeIframeHeight);
 
-function(config, loadScript, reportError, facebookStr) {
-
-    function Facebook($adSlot, params) {
-        facebookTpl || (facebookTpl = template(facebookStr));
-        labelTpl || (labelTpl = template(labelStr));
+var data = {
+  placementID: '180444840287_10154600557405288',
+  format: 'native',
+  testmode: false,
+  onAdLoaded: function(placementID) {
+    var interim = document.querySelector('[data-placementid=180444840287_10154600557405288]');
+    var ad = document.getElementById(interim.getAttribute('data-nativeadid'));
+    if (ad) {
+      write(function () {
+        ad.style.display = 'block';
+        resolve(true);
+      });
+    } else {
+      resolve(false);
     }
+  }
+};
 
-var placementID = '180444840287_10154600557405288';
-
-function bland() {
-      return new Promise(function (resolve, reject) {
-            window.fbAsyncInit || (window.fbAsyncInit = function() {
-              FB.init({
-                appId      : '',
-                xfbml      : true,
-                version    : 'v2.8'
-              });
-              FB.AppEvents.logPageView();
-            };
-
-            FB.Event.subscribe(
-              'ad.loaded',
-              function(placementID) {
-                var interim = document.querySelector('[data-placementid=' + placementID +']');
-                var ad = document.getElementById(interim.getAttribute('data-nativeadid'));
-                if (ad) {
-                  write(function () {
-                    ad.style.display = 'block';
-                    resolve(true);
-                  });
-                } else {
-                  resolve(false);
-                }
-              }
-            );
-
-                FB.Event.subscribe(
-                    'ad.error',
-                    function(errorCode, errorMessage, placementID) {
-                        reportError(new Error('Facebook returned an empty ad response'), {
-                            feature: 'commercial',
-                            placementID: placementID,
-                            errorMessage: errorMessage
-                        }, false);
-                        reject();
-                    }
-                );
-            });
-
-            var markup = facebookTpl(assign({ externalLink: svgs('externalLink'), testMode: this.params.testMode }, adUnits[this.params.placement]));;
-        }.bind(this));
-    };
-
-}
+(function(w,l,d,t){var a=t();var b=d.currentScript||(function(){var c=d.getElementsByTagName('script');return c[c.length-1];})();var e=b.parentElement;e.dataset.placementid=data.placementid;var f=function(v){try{return v.document.referrer;}catch(e){}return'';};var g=function(h){var i=h.indexOf('/',h.indexOf('://')+3);if(i===-1){return h;}return h.substring(0,i);};var j=[l.href];var k=false;var m=false;if(w!==w.parent){var n;var o=w;while(o!==n){var h;try{m=m||(o.$sf&&o.$sf.ext);h=o.location.href;}catch(e){k=true;}j.push(h||f(n));n=o;o=o.parent;}}var p=l.ancestorOrigins;if(p){if(p.length>0){data.domain=p[p.length-1];}else{data.domain=g(j[j.length-1]);}}data.url=j[j.length-1];data.channel=g(j[0]);data.width=screen.width;data.height=screen.height;data.pixelratio=w.devicePixelRatio;data.placementindex=w.ADNW&&w.ADNW.Ads?w.ADNW.Ads.length:0;data.crossdomain=k;data.safeframe=!!m;var q={};q.iframe=e.firstElementChild;var r='https://www.facebook.com/audiencenetwork/web/?sdk=5.3';for(var s in data){q[s]=data[s];if(typeof(data[s])!=='function'){r+='&'+s+'='+encodeURIComponent(data[s]);}}q.iframe.src=r;q.tagJsInitTime=a;q.rootElement=e;q.events=[];w.addEventListener('message',function(u){if(u.source!==q.iframe.contentWindow){return;}u.data.receivedTimestamp=t();if(this.sdkEventHandler){this.sdkEventHandler(u.data);}else{this.events.push(u.data);}}.bind(q),false);q.tagJsIframeAppendedTime=t();w.ADNW=w.ADNW||{};w.ADNW.Ads=w.ADNW.Ads||[];w.ADNW.Ads.push(q);w.ADNW.init&&w.ADNW.init(q);})(window,location,document,Date.now||function(){return+new Date;});
