194cf98f724c1d5e27e97afd7a63e34cd7854638
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-11-14 10:21:01 +0000

    Refactor Frame (web)

--- src/frame/web/index.js
+++ src/frame/web/index.js
@@ -1,8 +1,9 @@
-import { getIframeId, getWebfonts, resizeIframeHeight, onScroll, onViewport, reportClicks } from '../../_shared/js/messages';
+import { getIframeId, getWebfonts, reportClicks } from '../../_shared/js/messages';
 import { enableToggles } from '../../_shared/js/ui';
 
 enableToggles();
-reportClicks();
 getIframeId()
-.then(() => getWebfonts())
-.then(resizeIframeHeight);
+.then(() => {
+    reportClicks();
+    getWebfonts();
+});
