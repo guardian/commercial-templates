54835b9ce836b78bba79cafdf85f9cd366935f46
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2016-12-08 18:22:26 +0000

    New capi driven multi-container for Hosted

--- src/_shared/js/capi-multiple.js
+++ src/_shared/js/capi-multiple.js
@@ -23,7 +23,7 @@ function retrieveCapiData () {
         if (url !== '') {
             params.append('t', url);
         }
-    })
+    });
     return fetch(`${ENDPOINT}?${params}`)
     .then(response => response.json());
 }
@@ -82,9 +82,8 @@ function importCard (adType) {
 }
 
 // Constructs an individual card.
-function buildCard (cardInfo, cardNum, isPaid) {
+function buildCard (cardInfo, cardNum, adType) {
 
-    let adType = isPaid ? 'paidfor' : 'supported';
     let cardFragment = importCard(adType);
     let card = cardFragment.querySelector(`.advert--${adType}`);
     let imgContainer = card.querySelector('.advert__image-container');
@@ -95,12 +94,12 @@ function buildCard (cardInfo, cardNum, isPaid) {
     let image = generatePicture({
         url: OVERRIDES.images[cardNum] || cardInfo.articleImage.backupSrc,
         classes: ['advert__image'],
-        sources: cardInfo.articleImage.sources
+        sources: !OVERRIDES.images[cardNum] && cardInfo.articleImage.sources
     });
 
     imgContainer.insertAdjacentHTML('afterbegin', image);
 
-    if (cardNum > 0) {
+    if (cardNum > 0 && adType !== "hosted") {
         imgContainer.classList.add('hide-until-tablet');
     }
 
@@ -120,22 +119,22 @@ function addBranding (brandingCard) {
 }
 
 // Sets correct glabs link based on edition (AU/All others).
-function editionLink (host, edition, isPaid) {
+function editionLink (host, edition, adType) {
 
-    if (isPaid) {
+    if (adType === "paidfor") {
         setEditionLink(host, edition, document.querySelector('.adverts__stamp a'));
     }
 
 }
 
 // Uses cAPI data to build the ad content.
-function buildFromCapi (host, cardsInfo, isPaid) {
+function buildFromCapi (host, cardsInfo, adType) {
 
     let cardList = document.createDocumentFragment();
 
     // Constructs an array of cards from an array of data.
     cardsInfo.articles.forEach((info, idx) => {
-        cardList.appendChild(buildCard(info, idx, isPaid));
+        cardList.appendChild(buildCard(info, idx, adType));
     });
 
     return write(() => {
@@ -143,21 +142,21 @@ function buildFromCapi (host, cardsInfo, isPaid) {
         addBranding(cardsInfo.articles.slice(-1)[0]);
         let advertRow = document.querySelector('.adverts__row');
         advertRow.appendChild(cardList);
-        editionLink(host, cardsInfo.articles[0].edition, isPaid);
+        editionLink(host, cardsInfo.articles[0].edition, adType);
     });
 }
 
-export default function capiMultiple (isPaid) {
+export default function capiMultiple (adType) {
     let lastWidth;
 
     enableToggles();
 
-    getIframeId()
+    return getIframeId()
     .then(({ host }) => Promise.all([
         reportClicks(),
         getWebfonts(),
         retrieveCapiData()
-        .then(capiData => buildFromCapi(host, capiData, isPaid))
+        .then(capiData => buildFromCapi(host, capiData, adType))
     ]))
     .then(() => {
         onViewport(({ width }) => {
