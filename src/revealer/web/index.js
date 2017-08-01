ceafe4beaf121a984e0bb0cf23adbeb5a3aa4ee5
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-08-01 10:58:41 +0100

    Use messenger to define fixed position background

--- src/revealer/web/index.js
+++ src/revealer/web/index.js
@@ -1,9 +1,21 @@
-/*
-const background = this.adSlot.getElementsByClassName(
-    'creative__background'
-)[0];
-// for the height, we need to account for the height of the location bar, which
-// may or may not be there. 70px padding is not too much.
-if (background && viewport) {
-    background.style.height = `${viewport.height + 70}px`;
-}*/
+import { getIframeId, sendMessage } from '../../_shared/js/messages.js';
+
+getIframeId()
+.then(() => {
+
+    const [ scrollType, backgroundColour, backgroundImage, backgroundRepeat, backgroundPosition ] = [
+        'fixed',
+        '#f6f6f6',
+        `url('[%BackgroundImage%]')`,
+        'no-repeat',
+        'center center'
+    ];
+
+    sendMessage('background', {
+        scrollType,
+        backgroundColour,
+        backgroundImage,
+        backgroundRepeat,
+        backgroundPosition
+    });
+});
