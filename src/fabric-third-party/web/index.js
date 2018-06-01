f357336dedea6dd19d362b246913dbc82493b104
Francesca Hammond <francesca.hammond@guardian.co.uk>
2018-06-01 16:52:06 +0100

    Create a Fabric template to serve third party tags

---
+++ src/fabric-third-party/web/index.js
@@ -0,0 +1,36 @@
+//@flow
+import { getIframeId, resizeIframeHeight, getWebfonts } from '../../_shared/js/messages';
+import { write } from '../../_shared/js/dom';
+
+const ThirdPartyTag = '[%ThirdPartyTag%]';
+
+const ifThenPromise = (parameter, action) => {
+  if (parameter === 'true') {
+    return action();
+  } else {
+    return Promise.resolve();
+  }
+}
+
+const fetchTag = (tagUrl: string) => {
+  return fetch(tagUrl)
+    .then(response => response.text())
+}
+
+const generateTag = () => {
+  return fetchTag(`[%ThirdPartyTag%]`)
+}
+
+const insertTag = (tag) => {
+  const placeholder = document.getElementById('js-fabric-custom-third-party');
+  const range = document.createRange();
+  range.setStart(placeholder, 0);
+  range.setEnd(placeholder, 0);
+  placeholder.appendChild(range.createContextualFragment(tag));
+};
+
+getIframeId()
+.then(() => getWebfonts())
+.then(() => generateTag())
+.then(tag => insertTag(tag))
+.then(() => resizeIframeHeight());
