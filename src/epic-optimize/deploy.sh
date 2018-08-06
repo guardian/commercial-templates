9fa786c946114fa410a149de594de22452b48c05
Joseph Smith <joseph.smith@guardian.co.uk>
2018-08-06 19:29:26 +0100

    Set cache-control and x-amz-meta-surrogate-control headers

--- src/epic-optimize/deploy.sh
+++ src/epic-optimize/deploy.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 
 DIR=$(dirname "$0")
 cd "${DIR}/../.."
@@ -6,12 +7,22 @@ npm run build
 
 upload_build_file () {
     FILE="$1"
-    aws s3 cp --profile membership --region eu-west-1 --acl public-read "./build/epic-optimize/web/${FILE}" "s3://reader-revenue-components/epic/v1/${FILE}"
+    aws s3 cp \
+        --profile membership \
+        --region eu-west-1 \
+        --acl public-read "./build/epic-optimize/web/${FILE}" "s3://reader-revenue-components/epic/control/${FILE}" \
+        --cache-control 60 \
+        --metadata '{"x-amz-meta-surrogate-control":"3600"}'
 }
 
 upload_image () {
     FILE="paypal-and-credit-card.png"
-    aws s3 cp --profile membership --region eu-west-1 --acl public-read "./src/epic-optimize/images/${FILE}" "s3://reader-revenue-components/epic/v1/${FILE}"
+    aws s3 cp \
+        --profile membership \
+        --region eu-west-1 \
+        --acl public-read "./src/epic-optimize/images/${FILE}" "s3://reader-revenue-components/epic/control/${FILE}" \
+        --cache-control 60 \
+        --metadata '{"x-amz-meta-surrogate-control":"3600"}'
 }
 
 upload_build_file "index.html"
