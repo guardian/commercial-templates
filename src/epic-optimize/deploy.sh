75cbda52dbb079f7ec87ceede5eb75d18acd1e89
Joseph Smith <joseph.smith@guardian.co.uk>
2018-08-07 13:06:49 +0100

    Make purging slightly safer

--- src/epic-optimize/deploy.sh
+++ src/epic-optimize/deploy.sh
@@ -6,9 +6,9 @@ cd "${DIR}/../.."
 npm run build
 
 purge_cache () {
-    PATH="$1"
-    curl -X PURGE "https://support.code.dev-theguardian.com${PATH}"
-    curl -X PURGE "https://support.theguardian.com${PATH}"
+    FILE="$1"
+    curl -X PURGE "https://support.code.dev-theguardian.com/epic/control/${FILE}"
+    curl -X PURGE "https://support.theguardian.com/epic/control/${FILE}"
 }
 
 upload_build_file () {
@@ -19,7 +19,7 @@ upload_build_file () {
         --acl public-read "./build/epic-optimize/web/${FILE}" "s3://reader-revenue-components/epic/control/${FILE}" \
         --cache-control 60 \
         --metadata '{"surrogate-control":"86400"}'
-    purge_cache "/epic/control/${FILE}"
+    purge_cache ${FILE}
 }
 
 upload_image () {
@@ -30,7 +30,7 @@ upload_image () {
         --acl public-read "./src/epic-optimize/images/${FILE}" "s3://reader-revenue-components/epic/control/${FILE}" \
         --cache-control 60 \
         --metadata '{"surrogate-control":"86400"}'
-    purge_cache "/epic/control/${FILE}"
+    purge_cache ${FILE}
 }
 
 upload_build_file "index.html"
