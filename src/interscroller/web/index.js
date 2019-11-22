87eee8ed0bab7afac3cc1d468b68e88f1400142b
Ioanna Kyprianou <ioanna.kyprianou.contractor@guardian.co.uk>
2019-11-22 15:20:15 +0000

    Add interscroller native format

---
+++ src/interscroller/web/index.js
@@ -0,0 +1,30 @@
+import { getIframeId, sendMessage, onViewport } from '../../_shared/js/messages.js';
+import { once } from '../../_shared/js/utils';
+
+const updateBackground = () => {
+
+    const [ scrollType,
+        backgroundImage,
+        backgroundRepeat,
+        backgroundPosition,
+        backgroundSize] = [
+        'interscroller',
+        `url('[%BackgroundImage%]')`,
+        'no-repeat',
+        'center center',
+        'cover',
+    ];
+
+    sendMessage('background', {
+        scrollType,
+        backgroundImage,
+        backgroundRepeat,
+        backgroundPosition,
+        backgroundSize
+    });
+};
+
+getIframeId()
+.then(() => {
+    onViewport(once(updateBackground));
+});
