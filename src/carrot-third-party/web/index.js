83a29a531072127a5b5f1f0fd276774438b6b170
Ioanna Kyprianou <ioanna.kyprianou.contractor@guardian.co.uk>
2019-12-03 11:42:42 +0000

    carrot third party native ad

---
+++ src/carrot-third-party/web/index.js
@@ -0,0 +1,13 @@
+import {
+  getIframeId,
+  getWebfonts,
+  resizeIframeHeight,
+  reportClicks
+} from '../../_shared/js/messages.js';
+import { write } from '../../_shared/js/dom.js';
+
+reportClicks();
+
+getIframeId()
+.then(() => getWebfonts())
+.then(() => resizeIframeHeight());
