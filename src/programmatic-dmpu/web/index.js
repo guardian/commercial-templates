3d7ee976c5b3bc6fadf50d169d0ec1ee2a68c329
Scott Wolfe <scott_wolfe@33935.gnm.int>
2021-11-01 15:46:02 +0000

    removed unused import

--- src/programmatic-dmpu/web/index.js
+++ src/programmatic-dmpu/web/index.js
@@ -1,6 +1,5 @@
 //@flow
 import { getIframeId, resizeIframeHeight, getWebfonts } from '../../_shared/js/messages';
-import { write } from '../../_shared/js/dom';
 
 getIframeId()
 .then(() => getWebfonts())
