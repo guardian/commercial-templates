61b7e9ea24164a6defac28a5841c4d4813e4fa55
Max Duval <max.duval@theguardian.com>
2021-12-14 17:46:14 +0000

    initial sveltekit install

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export const templates = ['test']
+</script>
+
+<h1>This will handle templates</h1>
+
+<ol>
+    {#each templates as template}
+        <li><a href={`/${template}`}>{template.slice(0,1).toUpperCase() + template.slice(1)}</a></li>
+    {/each}
+</ol>
