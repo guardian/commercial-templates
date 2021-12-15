54a683723fe5a3f3dc2fa6b132587c8bdd6cd263
Max Duval <max.duval@theguardian.com>
2021-12-15 14:12:32 +0000

    format

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-    export const templates = ['test']
+	export const templates = ['test'];
 </script>
 
 <h1>Commercial Templates</h1>
 
 <ol>
-    {#each templates as template}
-        <li><a href={`/${template}`}>{template.slice(0,1).toUpperCase() + template.slice(1)}</a></li>
-    {/each}
+	{#each templates as template}
+		<li>
+			<a href={`/${template}`}>{template.slice(0, 1).toUpperCase() + template.slice(1)}</a>
+		</li>
+	{/each}
 </ol>
