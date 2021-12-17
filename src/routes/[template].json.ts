f2e87b45f9c464e05cbe05b6e991020122e4b4e8
Max Duval <max.duval@theguardian.com>
2021-12-17 02:00:35 +0000

    extract some functions

--- src/routes/[template].json.ts
+++ src/routes/[template].json.ts
@@ -1,39 +1,85 @@
 import fs from 'fs';
+import vm from 'vm';
 import alias from '@rollup/plugin-alias';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import type { RequestHandler } from '@sveltejs/kit/types';
-import type { ReadCommitResult } from 'isomorphic-git';
-import { log } from 'isomorphic-git';
-import type { RollupCache } from 'rollup';
+import { log, ReadCommitResult } from 'isomorphic-git';
+import type { Plugin, RollupCache } from 'rollup';
 import { rollup } from 'rollup';
 import svelte from 'rollup-plugin-svelte';
 import { terser } from 'rollup-plugin-terser';
 import preprocess from 'svelte-preprocess';
 
 const caches: Partial<Record<string, RollupCache>> = {};
-
 type Output = {
-	code: string;
-	commit: ReadCommitResult;
+	js: string;
+	css: string;
 };
 
 const github = 'https://github.com/guardian/commercial-templates/blob';
 
-const filepath = (template: string): `src/templates/${string}/index.ts` =>
-	`src/templates/${template}/index.ts`;
+const filepath = (template: string): `src/templates/${string}/index.svelte` =>
+	`src/templates/${template}/index.svelte`;
 
-const build = async (template: string): Promise<Output> => {
+const virtual = (template: string): Plugin => ({
+	name: 'virtual-template',
+	resolveId: (source: string) => {
+		if (source === 'ssr' || source === 'dom') return source;
+		return null;
+	},
+	load: (id: string) => {
+		if (id === 'ssr') {
+			return [
+				`import Template from "./${filepath(template)}"`,
+				`setShare(Template.render(${JSON.stringify(props)}))`,
+			].join('\n');
+		}
+		if (id === 'dom') {
+			return `import Template from "./${filepath(template)}";
+new Template({
+	target: document.querySelector('#svelte'),
+	props: {
+		info: '[%info%]',
+		brand: '[%brand%]',
+	},
+});`;
+		}
+		return null;
+	},
+});
+
+const getCommit = async (filepath: string): Promise<ReadCommitResult> => {
+	const dir = process.cwd();
+
+	const [commit] = await log({
+		fs,
+		dir,
+		ref: 'HEAD',
+		filepath,
+		force: true,
+	});
+
+	return commit;
+};
+
+const build = async (template: string, ssr: boolean): Promise<Output> => {
 	caches[template]
 		? console.info(`Building “${template}” from rollup cache`)
 		: console.warn(`Building “${template}” from fresh`);
 
 	const bundle = await rollup({
-		input: filepath(template),
+		input: 'dom',
 		cache: caches[template],
 		plugins: [
+			virtual(template),
 			svelte({
 				preprocess: preprocess(),
 				emitCss: false, // TODO, add css plugin for rollup
+				compilerOptions: {
+					generate: 'dom',
+					immutable: true,
+					hydratable: false,
+				},
 			}),
 			alias({
 				entries: [
@@ -54,38 +100,30 @@ const build = async (template: string): Promise<Output> => {
 	// Cache build for subsequent calls!
 	caches[template] = bundle.cache;
 
-	const dir = process.cwd();
-
-	const [commit] = await log({
-		fs,
-		dir,
-		ref: 'HEAD',
-		filepath: filepath(template),
-		force: true,
-	});
-
 	return {
-		code: output[0].code,
-		commit: commit,
+		js: output[0].code,
+		css: 'TBC',
 	};
 };
 
 export const get: RequestHandler = async ({ params }) => {
 	const { template } = params;
 
-	const js = await build(template);
-	const props = /props:({.+?})/.exec(js.code)?.[1] ?? '';
+	const client = await build(template, false);
+	const props = /props:({.+?})/.exec(client.js)?.[1] ?? '';
 
-	const sha = js.commit.oid.slice(0, 9);
+	const commit = await getCommit(filepath(template));
+	const sha = commit.oid.slice(0, 9);
 	const link = `${github}/${sha}/${filepath(template)}`;
-	const date = new Date(js.commit.commit.author.timestamp * 1_000)
+	const date = new Date(commit.commit.author.timestamp * 1_000)
 		.toISOString()
 		.slice(0, 10);
 
 	const html = [
 		`<!-- "${template}" updated on ${date} via ${link} -->`,
-		`<div id="svelte" data-template-id="${template}"></div>`,
-		`<script>${js.code}</script>`,
+		`<div id="svelte" data-template-id="${template}">`,
+		`</div>`,
+		`<script>${String(client.js)}</script>`,
 	].join('\n');
 
 	return {
@@ -96,3 +134,5 @@ export const get: RequestHandler = async ({ params }) => {
 		},
 	};
 };
+
+export { getCommit, virtual };
