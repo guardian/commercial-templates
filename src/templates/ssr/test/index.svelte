32650cc8cfa669a3b23c0b82cbca51e5d098a856
Max Duval <max.duval@theguardian.com>
2021-12-21 13:18:56 +0000

    remove broken TS lang in svelte

--- src/templates/ssr/test/index.svelte
+++ src/templates/ssr/test/index.svelte
@@ -1,11 +1,12 @@
 <script context="module" lang="ts">
+	import List from '$templates/components/List.svelte';
+
 	export const cdn = 'https://i.guim.co.uk/img/media/';
 	export const path =
 		'a5103656f1a074a716b8821afca8ee7c23361866/0_153_5347_3210/master/5347.jpg?width=620&quality=45&auto=format&fit=max&dpr=2&s=e156e68276b22fcf872a20a766cb326f';
-	import List from '$templates/components/List.svelte';
 </script>
 
-<script lang="ts">
+<script>
 	export let info;
 	export let brand;
 </script>
@@ -18,10 +19,9 @@
 	<img src={cdn + path} alt="an advertisment" />
 </div>
 
-<List sizes={[300,600,900,1200]}/>
+<List sizes={[300, 600, 900, 1200]} />
 
 <style>
-
 	.wrapper {
 		background-color: var(--brand);
 	}
