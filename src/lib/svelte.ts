3d5f522bfa59b78e4b070907f3c364e214e15517
Max Duval <max.duval@theguardian.com>
2022-01-06 12:09:57 +0000

    make the build work
    Co-authored-by: Chris Jones <chrislomaxjones96@gmail.com>

--- src/lib/svelte.ts
+++ src/lib/svelte.ts
@@ -1,15 +1,10 @@
 import { gamVar } from '$lib/gam';
 import { readFileSync } from 'fs';
-import { sveltePreprocess } from 'svelte-preprocess/dist/autoProcess';
-import { compile, preprocess } from 'svelte/compiler';
+import { compile } from 'svelte/compiler';
 
 type Props = Record<string, `[%${string}%]`>;
 
 const getProps = (path: string): Props => {
-	// const { code } = await preprocess(readFileSync(path, 'utf8'), [
-	// 	sveltePreprocess(),
-	// ]);
-
 	const { vars } = compile(
 		// TODO: handle Typescript, too
 		readFileSync(path, 'utf8'),
