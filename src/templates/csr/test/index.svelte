c0c70a29bf008386e530c42d0d6297f0bf81e391
Max Duval <max.duval@theguardian.com>
2022-01-17 16:12:52 +0000

    add types to test

--- src/templates/csr/test/index.svelte
+++ src/templates/csr/test/index.svelte
@@ -6,9 +6,11 @@
 		'a5103656f1a074a716b8821afca8ee7c23361866/0_153_5347_3210/master/5347.jpg?width=620&quality=45&auto=format&fit=max&dpr=2&s=e156e68276b22fcf872a20a766cb326f';
 </script>
 
-<script>
-	export let Info;
-	export let Brand;
+<script lang="ts">
+	import type { Prop } from '$lib/svelte';
+
+	export let Info: Prop;
+	export let Brand: Prop;
 </script>
 
 <div class="wrapper" style={`--brand:${Brand}`}>
