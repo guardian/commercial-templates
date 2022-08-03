7193d0d740c3bcb928afbbfc379175e505afc1dd
Emma Imber <emma-jo.imber@guardian.co.uk>
2022-08-03 17:55:36 +0100

    Add linting exception for vite.config.js

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,12 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	extends: ['@guardian/eslint-config-typescript'],
 	plugins: ['svelte3', '@typescript-eslint', 'import'],
-	ignorePatterns: ['*.cjs', 'svelte.config.js', 'legacy/**'],
+	ignorePatterns: [
+		'*.cjs',
+		'svelte.config.js',
+		'vite.config.js',
+		'legacy/**',
+	],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
 		'svelte3/typescript': () => require('typescript'),
