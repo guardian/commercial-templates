5a73a2e8de6dc8fdc0c9227110fa81e0c0543296
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-08-03 17:08:22 +0100

    move ts back to index.ts

---
+++ src/templates/ssr/interscroller/index.ts
@@ -0,0 +1,23 @@
+import { post } from '$lib/messenger';
+
+post({
+	type: 'background',
+	value: {
+		scrollType: 'interscroller',
+		backgroundImage: `url('[%BackgroundImage%]')`,
+		backgroundRepeat: 'no-repeat',
+		backgroundPosition: 'center center',
+		backgroundSize: 'cover',
+		ctaUrl: `%%CLICK_URL_UNESC%%%%DEST_URL%%`,
+	},
+});
+
+post({
+	type: 'set-ad-height',
+	value: {
+		width: -1,
+		height: '85vh',
+	},
+});
+
+export {};
