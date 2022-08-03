a930b1a0cd0fd3fda0da5bbdc9586c759cf6610b
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-08-03 16:02:40 +0100

    remove index.ts from interscroller

--- src/templates/ssr/interscroller/index.svelte
+++ src/templates/ssr/interscroller/index.svelte
@@ -1,5 +1,26 @@
 <!-- https://polyfill.io/v3/polyfill.min.js?features=default -->
 <script lang="ts">
+	import { post } from '$lib/messenger';
+
+	post({
+		type: 'background',
+		value: {
+			scrollType: 'interscroller',
+			backgroundImage: `url('[%BackgroundImage%]')`,
+			backgroundRepeat: 'no-repeat',
+			backgroundPosition: 'center center',
+			backgroundSize: 'cover',
+			ctaUrl: `%%CLICK_URL_UNESC%%%%DEST_URL%%`,
+		},
+	});
+
+	post({
+		type: 'set-ad-height',
+		value: {
+			width: -1,
+			height: '85vh',
+		},
+	});
 </script>
 
 <div class="creative--interscroller">
