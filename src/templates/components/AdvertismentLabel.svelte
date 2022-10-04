ae0a135f0f96013faf9a5af0db97f93c12c23311
Onelittlewolfe <onelittlewolfe@gmail.com>
2022-10-04 16:45:36 +0100

    advertismet label as its own componenet and reverting picture element to original background image for layers

---
+++ src/templates/components/AdvertismentLabel.svelte
@@ -0,0 +1,9 @@
+<script lang="ts"></script>
+
+<div class="gs-container creative__label creative__label">
+	<div class="creative__label-text">Advertisement</div>
+</div>
+
+<style lang="scss">
+	@import './src/lib/scss/core';
+</style>
