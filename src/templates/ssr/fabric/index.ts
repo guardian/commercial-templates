6ec23a477145aba05eecf0e242aa188814bbe25d
Onelittlewolfe <onelittlewolfe@gmail.com>
2022-10-18 12:13:31 +0100

    Refactor of if mobile logic and simplyfying parallax background
    refactor check if mobile and change backgroundColour var to backgroundColor
    
    simplfying the logic for background parallax. Previoulsy we didn't have parallax option if mobile

--- src/templates/ssr/fabric/index.ts
+++ src/templates/ssr/fabric/index.ts
@@ -2,18 +2,18 @@ import { write } from '$lib/dom';
 import type { GAMVariable } from '$lib/gam';
 import { post } from '$lib/messenger';
 
+type ParallaxBackgroundSize = '740px 500px' | '1300px 700px';
 const linkDesktop: HTMLElement | null = document.getElementById('linkDesktop');
-
 const isMobile: boolean = window.matchMedia('(max-width: 739px)').matches;
-const isTablet: boolean = window.matchMedia(
-	'(min-width: 740px) and (max-width: 979px)',
-).matches;
-handleBackground(isMobile, isTablet);
+
+const backgroundSize: ParallaxBackgroundSize = isMobile
+	? '740px 500px'
+	: '1300px 700px';
 
 //creative background div for parallax scrolling
-function handleBackground(isMobile: boolean, isTablet: boolean) {
+const handleBackground = (isMobile: boolean) => {
 	const scrollType: GAMVariable = '[%BackgroundScrollType%]';
-	const backgroundColour: GAMVariable | null = '[%BackgroundColour%]';
+	const backgroundColor: GAMVariable | null = '[%BackgroundColor%]';
 	const [backgroundImage, backgroundPosition, backgroundRepeat, creativeLink] =
 		isMobile
 			? [
@@ -29,10 +29,10 @@ function handleBackground(isMobile: boolean, isTablet: boolean) {
 					document.getElementById('linkDesktop'),
 			  ];
 
-	document.documentElement.style.backgroundColor = backgroundColour;
+	document.documentElement.style.backgroundColor = backgroundColor;
 
 	if (!backgroundImage) return;
-	if (scrollType.includes('none')) {
+	if (scrollType.includes('none') && creativeLink) {
 		void write(() => {
 			Object.assign(creativeLink.style, {
 				backgroundImage: `url('${backgroundImage}')`,
@@ -40,23 +40,29 @@ function handleBackground(isMobile: boolean, isTablet: boolean) {
 				backgroundRepeat,
 			});
 		});
-	} else {
+	}
+	//Wanted to just use background-size:cover here, but parallax effect does not
+	//work on mobile, setting px sizes was the soloution
+	if (scrollType.includes('parallax')) {
 		post({
 			type: 'background',
 			value: {
-				scrollType:
-					scrollType.includes('parallax') && (isMobile || isTablet)
-						? 'fixed'
-						: scrollType,
-				backgroundColour,
+				scrollType: scrollType,
+				backgroundColor,
 				backgroundImage: `url('${backgroundImage}')`,
 				backgroundRepeat,
 				backgroundPosition,
+				backgroundSize,
 			},
 		});
+
 		//css class added to fix padding issue
-		linkDesktop ? linkDesktop.classList.add('is-parallax') : '';
+		if (linkDesktop && scrollType.includes('parallax')) {
+			linkDesktop.classList.add('is-parallax');
+		}
 	}
-}
+};
+
+handleBackground(isMobile);
 
 export {};
