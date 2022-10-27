607fa4130d3c1958244b4e7620b9f4bc7a2c042d
Max Duval <max.duval@theguardian.com>
2022-10-27 16:49:40 +0100

    feat: add ThirdPartyScript

---
+++ src/templates/components/ThirdPartyScript.svelte
@@ -0,0 +1,16 @@
+<script
+	context="module"
+	src="https://pixel.adsafeprotected.com/rjss/st/726370/54949606/skeleton.js"
+>
+	// @ts-nocheck
+</script>
+
+<noscript>
+	<!-- svelte-ignore missing-declaration -->
+	<img
+		SRC="https://pixel.adsafeprotected.com/rfw/st/726370/54949605/skeleton.gif?gdpr=${GDPR}&gdpr_consent=${GDPR_CONSENT_278}&gdpr_pd=${GDPR_PD}"
+		width="1"
+		height="1"
+		alt=""
+	/>
+</noscript>
