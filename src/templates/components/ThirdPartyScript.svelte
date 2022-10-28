e1eb20e7842d857b983c112b44b806df6cbfd5cc
Max Duval <max.duval@theguardian.com>
2022-10-28 17:06:28 +0100

    fix: ThirdPartyScript

--- src/templates/components/ThirdPartyScript.svelte
+++ src/templates/components/ThirdPartyScript.svelte
@@ -1,16 +1,15 @@
-<script
-	context="module"
-	src="https://pixel.adsafeprotected.com/rjss/st/726370/54949606/skeleton.js"
->
-	// @ts-nocheck
+<script>
+	const scriptSource =
+		'https://pixel.adsafeprotected.com/rjss/st/726370/54949606/skeleton.js';
+
+	const imageSource =
+		'https://pixel.adsafeprotected.com/rfw/st/726370/54949605/skeleton.gif?gdpr=${GDPR}&gdpr_consent=${GDPR_CONSENT_278}&gdpr_pd=${GDPR_PD}';
 </script>
 
+<svelte:head>
+	<script src={scriptSource}></script>
+</svelte:head>
+
 <noscript>
-	<!-- svelte-ignore missing-declaration -->
-	<img
-		SRC="https://pixel.adsafeprotected.com/rfw/st/726370/54949605/skeleton.gif?gdpr=${GDPR}&gdpr_consent=${GDPR_CONSENT_278}&gdpr_pd=${GDPR_PD}"
-		width="1"
-		height="1"
-		alt=""
-	/>
+	<img src={imageSource} width="1" height="1" alt="" />
 </noscript>
