9812d67ffeaf9d6f0b8333a8edec76d1e79fadfc
Jake Lee Kennedy <jlkennedy127@gmail.com>
2023-07-20 13:52:31 +0100

    expand public good api

--- src/lib/public-good.ts
+++ src/lib/public-good.ts
@@ -1,12 +1,30 @@
+/**
+ * https://assets.publicgood.com/docs/gen/pgmApi.html#create__anchor
+ */
 export interface PgmApiOptions {
 	partnerId: string;
 	attributes: {
+		targetId?: string;
+		targetType?: string;
 		url: string;
 	};
+	onShow?: () => void;
+	onHide?: () => void;
+}
+
+/**
+ * https://assets.publicgood.com/docs/gen/global.html#MatchInfo
+ */
+interface MatchInfo {
+	action: string;
+	cpm: number;
+	partner_id: string;
+	campaign_id: string;
 }
 
 export interface PgmApi {
 	create: (el: HTMLElement, options: PgmApiOptions) => void;
+	getAdvice(options: PgmApiOptions): Promise<MatchInfo>;
 }
 
 export const create = (el: HTMLElement, options: PgmApiOptions) => {
