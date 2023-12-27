dba760848e83627d0bdb0c102e802ed95289713a
Dina Hafez <dina.hafez@guardian.co.uk>
2023-12-27 17:26:38 +0000

    Add Pixel component and fabric index.svelte

--- src/templates/components/AdvertisementLabel.svelte
+++ src/templates/components/AdvertisementLabel.svelte
@@ -1,45 +1,51 @@
 <script lang="ts">
 </script>
 
-<div class="gs-container creative__label creative__label">
-	<div class="creative__label-text">Advertisement</div>
+<div class="gs-container label label">
+	<div class="label-text">Advertisement</div>
 </div>
 
 <style lang="scss">
+	@import './colours/palette.scss';
+	$gs-gutter: 20px;
+	$gs-max-columns: 16;
+	$f-sans-serif-text: 'Guardian Text Sans Web', 'Helvetica Neue', Helvetica,
+		Arial, 'Lucida Grande', sans-serif !default;
+
 	.gs-container {
 		position: relative;
 
-		// @include mq(tablet) {
-		// 	padding: 0 calc(50% - #{(gs-span(9) + $gs-gutter * 2) / 2});
-		// }
+		@media (min-width: 740px) {
+			padding: 0 calc(50% - #{(gs-span(9) + $gs-gutter * 2) / 2});
+		}
 
-		// @include mq(desktop) {
-		// 	padding: 0 calc(50% - #{(gs-span(12) + $gs-gutter * 2) / 2});
-		// }
+		@media (min-width: 980px) {
+			padding: 0 calc(50% - #{(gs-span(12) + $gs-gutter * 2) / 2});
+		}
 
-		// @include mq(leftCol) {
-		// 	padding: 0 calc(50% - #{(gs-span(14) + $gs-gutter * 2) / 2});
-		// }
+		@media (min-width: 1140px) {
+			padding: 0 calc(50% - #{(gs-span(14) + $gs-gutter * 2) / 2});
+		}
 
-		// @include mq(wide) {
-		// 	padding: 0 calc(50% - #{(gs-span($gs-max-columns) + $gs-gutter * 2) / 2});
-		// }
+		@media (min-width: 1300px) {
+			padding: 0 calc(50% - #{(gs-span($gs-max-columns) + $gs-gutter * 2) / 2});
+		}
 	}
 
-	.creative__label {
-		// background-color: $neutral-8;
-		// border-top: 1px solid $neutral-5;
+	.label {
+		background-color: $neutral-8;
+		border-top: 1px solid $neutral-5;
 		color: #6e6e6e;
-		// font: normal .75rem/1.9 $f-sans-serif-text;
+		font: normal 0.75rem/1.9 $f-sans-serif-text;
 		height: 2em;
 		box-sizing: border-box;
 	}
 
-	// .creative__label-text {
-	// 	padding: 0 $gs-gutter / 2;
+	.label-text {
+		padding: 0 $gs-gutter / 2;
 
-	// 	@include mq(tablet) {
-	// 		padding: 0 $gs-gutter;
-	// 	}
-	// }
+		@media (min-width: 740px) {
+			padding: 0 $gs-gutter;
+		}
+	}
 </style>
---
+++ src/templates/components/Pixel.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { GAMVariable } from '$lib/gam';
+
+	export let src: GAMVariable;
+</script>
+
+<img {src} alt="" class="pixel" />
+
+<style lang="scss">
+	.pixel {
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		width: 1px;
+		height: 1px;
+		opacity: 0;
+	}
+</style>
