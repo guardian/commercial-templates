1493c7bd4fa4f0522d03f756d882bb16d2d2c21e
Dina Hafez <dina.hafez@guardian.co.uk>
2023-12-27 12:48:21 +0000

    Add AdvertisementLabel component and test.json

---
+++ src/templates/components/AdvertisementLabel.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+</script>
+
+<div class="gs-container creative__label creative__label">
+	<div class="creative__label-text">Advertisement</div>
+</div>
+
+<style lang="scss">
+	.gs-container {
+		position: relative;
+
+		// @include mq(tablet) {
+		// 	padding: 0 calc(50% - #{(gs-span(9) + $gs-gutter * 2) / 2});
+		// }
+
+		// @include mq(desktop) {
+		// 	padding: 0 calc(50% - #{(gs-span(12) + $gs-gutter * 2) / 2});
+		// }
+
+		// @include mq(leftCol) {
+		// 	padding: 0 calc(50% - #{(gs-span(14) + $gs-gutter * 2) / 2});
+		// }
+
+		// @include mq(wide) {
+		// 	padding: 0 calc(50% - #{(gs-span($gs-max-columns) + $gs-gutter * 2) / 2});
+		// }
+	}
+
+	.creative__label {
+		// background-color: $neutral-8;
+		// border-top: 1px solid $neutral-5;
+		color: #6e6e6e;
+		// font: normal .75rem/1.9 $f-sans-serif-text;
+		height: 2em;
+		box-sizing: border-box;
+	}
+
+	// .creative__label-text {
+	// 	padding: 0 $gs-gutter / 2;
+
+	// 	@include mq(tablet) {
+	// 		padding: 0 $gs-gutter;
+	// 	}
+	// }
+</style>
