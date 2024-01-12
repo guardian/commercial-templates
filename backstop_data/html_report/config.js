59fb2b33d011a461343e7382dd8a2cf772e97eae
Emma Imber <emma-jo.imber@guardian.co.uk>
2024-01-12 15:34:10 +0000

    Add 5 templates for testing and add reference images

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_CAPI_Single_PaidFor_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20240112-124211/backstop_default_CAPI_Single_PaidFor_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20240112-153252/backstop_default_CAPI_Single_PaidFor_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "backstop_default_CAPI_Single_PaidFor_0_document_0_desktop.png",
         "label": "CAPI Single PaidFor",
@@ -14,24 +14,14 @@ report({
         "referenceUrl": "",
         "expect": 0,
         "viewportLabel": "desktop",
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "rawMisMatchPercentage": 2.108095646000216,
-          "misMatchPercentage": "2.11",
-          "analysisTime": 191
-        },
-        "diffImage": "../bitmaps_test/20240112-124211/failed_diff_backstop_default_CAPI_Single_PaidFor_0_document_0_desktop.png"
+        "error": "Reference file not found /Users/emma-jo_imber/code/commercial-templates/backstop_data/bitmaps_reference/backstop_default_CAPI_Single_PaidFor_0_document_0_desktop.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_CAPI_Multiple_PaidFor_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20240112-124211/backstop_default_CAPI_Multiple_PaidFor_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20240112-153252/backstop_default_CAPI_Multiple_PaidFor_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "backstop_default_CAPI_Multiple_PaidFor_0_document_0_desktop.png",
         "label": "CAPI Multiple PaidFor",
@@ -41,17 +31,58 @@ report({
         "referenceUrl": "",
         "expect": 0,
         "viewportLabel": "desktop",
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "rawMisMatchPercentage": 1.7776602744876857,
-          "misMatchPercentage": "1.78",
-          "analysisTime": 266
-        },
-        "diffImage": "../bitmaps_test/20240112-124211/failed_diff_backstop_default_CAPI_Multiple_PaidFor_0_document_0_desktop.png"
+        "error": "Reference file not found /Users/emma-jo_imber/code/commercial-templates/backstop_data/bitmaps_reference/backstop_default_CAPI_Multiple_PaidFor_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Events_Multiple_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20240112-153252/backstop_default_Events_Multiple_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Events_Multiple_0_document_0_desktop.png",
+        "label": "Events Multiple",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:7777/csr/events-multiple",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "error": "Reference file not found /Users/emma-jo_imber/code/commercial-templates/backstop_data/bitmaps_reference/backstop_default_Events_Multiple_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Manual_Multiple_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20240112-153252/backstop_default_Manual_Multiple_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Manual_Multiple_0_document_0_desktop.png",
+        "label": "Manual Multiple",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:7777/csr/manual-multiple",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "error": "Reference file not found /Users/emma-jo_imber/code/commercial-templates/backstop_data/bitmaps_reference/backstop_default_Manual_Multiple_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Manual_Single_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20240112-153252/backstop_default_Manual_Single_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Manual_Single_0_document_0_desktop.png",
+        "label": "Manual Single",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:7777/ssr/manual-single",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "error": "Reference file not found /Users/emma-jo_imber/code/commercial-templates/backstop_data/bitmaps_reference/backstop_default_Manual_Single_0_document_0_desktop.png"
       },
       "status": "fail"
     }
