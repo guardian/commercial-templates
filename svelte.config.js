6ee851a684935a9cee5f350fda60464bee231abc
Jake Lee Kennedy <jlkennedy127@gmail.com>
2024-01-12 18:00:55 +0000

    upgrade part 1

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const isDev = process.env.NODE_ENV === 'development';
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter(),
-
 		prerender: {
 			default: true,
 		},
