5f5f16c1764af069508d4e2f12e7221bed3e2fcf
Emma Imber <emma-jo.imber@guardian.co.uk>
2024-01-12 15:29:37 +0000

    Only show HTML and CSS on button press

--- src/lib/Code.svelte
+++ src/lib/Code.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	export let html: string;
 	export let css: string;
+	export let showHTML = false;
+	export let showCSS = false;
 </script>
 
 <section id="code">
@@ -8,11 +10,29 @@
 
 	<div class="html">
 		<h2>HTML</h2>
-		<pre>{html}</pre>
+		<button on:click={() => (showHTML = !showHTML)}>
+			{#if showHTML}
+				Hide HTML
+			{:else}
+				Show HTML
+			{/if}
+		</button>
+		{#if showHTML}
+			<pre>{html}</pre>
+		{/if}
 	</div>
 	<div>
 		<h2>CSS</h2>
-		<pre>{css}</pre>
+		<button on:click={() => (showCSS = !showCSS)}>
+			{#if showCSS}
+				Hide CSS
+			{:else}
+				Show CSS
+			{/if}
+		</button>
+		{#if showCSS}
+			<pre>{css}</pre>
+		{/if}
 	</div>
 </section>
 
@@ -30,5 +50,15 @@
 
 	h3 {
 		grid-column: span 2;
+	}
+
+	button {
+		color: white;
+		border: 2px solid darkblue;
+		background-color: darkblue;
+		border-radius: 15px;
+		font-size: 1rem;
+		font-family: inherit;
+		padding: 5px 10px;
 	}
 </style>
