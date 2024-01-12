c1e66386c95baa9a4b12edefaee7b448b3d925a1
Emma Imber <emma-jo.imber@guardian.co.uk>
2024-01-12 16:22:24 +0000

    Ask eslint to ignore backstop_data

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,25 @@ module.exports = {
 		'prettier',
 	],
 	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
+	extends: ['@guardian/eslint-config-typescript'],
+	plugins: ['svelte3', '@typescript-eslint', 'import'],
+	ignorePatterns: [
+		'*.cjs',
+		'svelte.config.js',
+		'vite.config.js',
+		'legacy/**',
+		'backstop_data/**',
+	],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	settings: {
+		'svelte3/typescript': () => require('typescript'),
+		'import/resolver': {
+			typescript: {},
+			node: {
+				extensions: ['.js', '.jsx', '.ts', '.tsx'],
+			},
+		},
+	},
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
