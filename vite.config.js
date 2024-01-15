08d4ed3854f03a942b9a8bec745b31d0ee10e2ff
Jake Lee Kennedy <jlkennedy127@gmail.com>
2024-01-15 09:49:14 +0000

    put template hot update back

--- vite.config.js
+++ vite.config.js
@@ -4,37 +4,37 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
 	plugins: [
 		sveltekit(),
-		// {
-		// 	name: 'watch-templates',
-		// 	configureServer(server) {
-		// 		server.watcher.add('/src/templates');
-		// 	},
-		// 	handleHotUpdate(ctx) {
-		// 		const TEMPLATE =
-		// 			/\/templates\/([\w-\/]+?)\/[\w-]+?\.(svelte|js|ts|md|css|json)$/i;
-		// 		const matches = TEMPLATE.exec(ctx.file);
+		{
+			name: 'watch-templates',
+			configureServer(server) {
+				server.watcher.add('/src/templates');
+			},
+			handleHotUpdate(ctx) {
+				const TEMPLATE =
+					/\/templates\/([\w-\/]+?)\/[\w-]+?\.(svelte|js|ts|md|css|json)$/i;
+				const matches = TEMPLATE.exec(ctx.file);
 
-		// 		if (!matches) return ctx.modules;
+				if (!matches) return ctx.modules;
 
-		// 		console.warn(
-		// 			`Template ${matches[1]} changed`,
-		// 			'sending template-update event',
-		// 		);
+				console.warn(
+					`Template ${matches[1]} changed`,
+					'sending template-update event',
+				);
 
-		// 		/** @type {import('./src/lib/reload').Data} */
-		// 		const data = {
-		// 			id: matches[1],
-		// 		};
+				/** @type {import('./src/lib/reload').Data} */
+				const data = {
+					id: matches[1],
+				};
 
-		// 		ctx.server.ws.send({
-		// 			type: 'custom',
-		// 			event: 'template-update',
-		// 			data,
-		// 		});
+				ctx.server.ws.send({
+					type: 'custom',
+					event: 'template-update',
+					data,
+				});
 
-		// 		return [];
-		// 	},
-		// },
+				return [];
+			},
+		},
 	],
 };
 
