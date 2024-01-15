33bf9db85c040fcc48d68ef9998375b2c408d457
Jake Lee Kennedy <jlkennedy127@gmail.com>
2024-01-15 16:33:24 +0000

    lint

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
 		},
 
 		alias: {
