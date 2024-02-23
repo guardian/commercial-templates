0cb1a3f5d272b111151e133d8914dce201fc0901
Emma Imber <emma-jo.imber@guardian.co.uk>
2024-02-23 12:21:23 +0000

    Fix eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,25 +9,8 @@ module.exports = {
 		'prettier',
 	],
 	parser: '@typescript-eslint/parser',
-	extends: ['@guardian/eslint-config-typescript'],
-	plugins: ['svelte3', '@typescript-eslint', 'import'],
-	ignorePatterns: [
-		'*.cjs',
-		'svelte.config.js',
-		'vite.config.js',
-		'legacy/**',
-		'backstop_data/**',
-	],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript'),
-		'import/resolver': {
-			typescript: {},
-			node: {
-				extensions: ['.js', '.jsx', '.ts', '.tsx'],
-			},
-		},
-	},
+	plugins: ['@typescript-eslint'],
+	ignorePatterns: ['backstop_data/**'],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
