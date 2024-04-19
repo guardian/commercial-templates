cbbd04c8a03af933db123e639e5ededba479c8b8
Emma Imber <emma-jo.imber@guardian.co.uk>
2024-04-19 13:23:51 +0100

    Remove unhelpful changes and update file reference

--- backstop_data/engine_scripts/puppet/onReady.cjs
+++ backstop_data/engine_scripts/puppet/onReady.cjs
@@ -1,5 +1,5 @@
 module.exports = async (page, scenario, vp) => {
-  await page.waitFor(() => {
+  await page.waitForTimeout(() => {
     return document.fonts.ready.then(() => {
       console.log('Fonts loaded');
       document.getElementsByTagName('body')[0].style['-webkit-font-smoothing'] = 'none';
