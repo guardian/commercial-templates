82679b4ed6edaf9d0d5fbc3023f6cdeef760b6c8
Emma Imber <emma-jo.imber@guardian.co.uk>
2024-07-05 17:06:47 +0100

    Refactor manual multiple test

--- playwright/manual-multiple.test.ts
+++ playwright/manual-multiple.test.ts
@@ -5,17 +5,17 @@ const viewport = { width: 1600, height: 1000 };
 const widths = ['360', '740', '980', '1300', '100%'];
 
 test.describe('Manual Multiple visual regression testing', () => {
-	for (const breakpoint of widths) {
-		test(`Manual Multiple: ${breakpoint}`, async ({ page }) => {
-			await page.setViewportSize(viewport);
+	test('Get reference screenshots', async ({ page }) => {
+		await page.setViewportSize(viewport);
 
-			await page.goto(
-				'https://guardian.github.io/commercial-templates/csr/manual-multiple/',
-				{
-					waitUntil: 'networkidle',
-				},
-			);
+		await page.goto(
+			'https://guardian.github.io/commercial-templates/csr/manual-multiple/',
+			{
+				waitUntil: 'networkidle',
+			},
+		);
 
+		for (const breakpoint of widths) {
 			const referenceTemplateLocator = page
 				.frameLocator(`[name='width-${breakpoint}']`)
 				.locator('html');
@@ -27,11 +27,17 @@ test.describe('Manual Multiple visual regression testing', () => {
 			await referenceTemplateLocator.screenshot({
 				path: `./playwright/reference-images/Manual-multiple-${breakpoint.replace('%', '')}.png`,
 			});
+		}
+	});
 
-			await page.goto('http://localhost:7777/csr/manual-multiple', {
-				waitUntil: 'networkidle',
-			});
+	test('Compare PR templates to reference screenshots', async ({ page }) => {
+		await page.setViewportSize(viewport);
 
+		await page.goto('http://localhost:7777/csr/manual-multiple', {
+			waitUntil: 'networkidle',
+		});
+
+		for (const breakpoint of widths) {
 			const testTemplateLocator = page
 				.frameLocator(`[name='width-${breakpoint}']`)
 				.locator('html');
@@ -44,6 +50,6 @@ test.describe('Manual Multiple visual regression testing', () => {
 				`Manual-multiple-${breakpoint.replace('%', '')}.png`,
 				{ maxDiffPixelRatio: 0.004 },
 			);
-		});
-	}
+		}
+	});
 });
