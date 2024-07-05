e0a360fca3159891dba7064b53876ffa8864f1ad
Emma Imber <emma-jo.imber@guardian.co.uk>
2024-07-05 15:36:57 +0100

    Manual inline visual test

---
+++ playwright/manual-inline.test.ts
@@ -0,0 +1,49 @@
+import { expect, test } from '@playwright/test';
+
+const viewport = { width: 1600, height: 1000 };
+
+const widths = ['360', '740', '980', '1300', '100%'];
+
+test.describe('Manual Inline visual regression testing', () => {
+	for (const breakpoint of widths) {
+		test(`Manual Inline: ${breakpoint}`, async ({ page }) => {
+			await page.setViewportSize(viewport);
+
+			await page.goto(
+				'https://guardian.github.io/commercial-templates/ssr/manual-inline/',
+				{
+					waitUntil: 'networkidle',
+				},
+			);
+
+			const referenceTemplateLocator = page
+				.frameLocator(`[name='width-${breakpoint}']`)
+				.locator('html');
+			// check that the template is present on the page
+			await referenceTemplateLocator.isVisible();
+			// scroll to it
+			await referenceTemplateLocator.scrollIntoViewIfNeeded();
+			// take a reference screenshot
+			await referenceTemplateLocator.screenshot({
+				path: `./playwright/reference-images/Manual-inline-${breakpoint.replace('%', '')}.png`,
+			});
+
+			await page.goto('http://localhost:7777/ssr/manual-inline', {
+				waitUntil: 'networkidle',
+			});
+
+			const testTemplateLocator = page
+				.frameLocator(`[name='width-${breakpoint}']`)
+				.locator('html');
+			// check that the template is present on the page
+			await testTemplateLocator.isVisible();
+			// scroll to it
+			await testTemplateLocator.scrollIntoViewIfNeeded();
+			// compare screenshot to reference
+			await expect(testTemplateLocator).toHaveScreenshot(
+				`Manual-inline-${breakpoint.replace('%', '')}.png`,
+				{ maxDiffPixelRatio: 0.004 },
+			);
+		});
+	}
+});
