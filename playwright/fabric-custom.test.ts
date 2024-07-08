71f70e334e18f03403dad025f0d7951bb5f32f59
Emma Imber <emma-jo.imber@guardian.co.uk>
2024-07-08 15:19:18 +0100

    Disable animations for fabric custom test screenshots

--- playwright/fabric-custom.test.ts
+++ playwright/fabric-custom.test.ts
@@ -6,9 +6,6 @@ const widths = ['360', '740', '980', '1300', '100%'];
 
 test.describe('Fabric Custom visual regression testing', () => {
 	test('Get reference screenshots', async ({ page }) => {
-		// increase default test timeout to accommodate waiting for creative to stop moving
-		test.setTimeout(60000);
-
 		await page.setViewportSize(viewport);
 
 		await page.goto(
@@ -17,9 +14,6 @@ test.describe('Fabric Custom visual regression testing', () => {
 				waitUntil: 'networkidle',
 			},
 		);
-
-		// the Fabric Custom has an animated creative, so we wait for it to stop moving before taking a screenshot
-		await page.waitForTimeout(15000);
 
 		for (const breakpoint of widths) {
 			const referenceTemplateLocator = page
@@ -31,23 +25,18 @@ test.describe('Fabric Custom visual regression testing', () => {
 			await referenceTemplateLocator.scrollIntoViewIfNeeded();
 			// take a reference screenshot
 			await referenceTemplateLocator.screenshot({
+				animations: 'disabled',
 				path: `./playwright/reference-images/Fabric-custom-${breakpoint.replace('%', '')}.png`,
 			});
 		}
 	});
 
 	test('Compare PR templates to reference screenshots', async ({ page }) => {
-		// increase default test timeout to accommodate waiting for creative to stop moving
-		test.setTimeout(60000);
-
 		await page.setViewportSize(viewport);
 
 		await page.goto('http://localhost:7777/ssr/fabric-custom', {
 			waitUntil: 'networkidle',
 		});
-
-		// the Fabric Custom has an animated creative, so we wait for it to stop moving before taking a screenshot
-		await page.waitForTimeout(15000);
 
 		for (const breakpoint of widths) {
 			const testTemplateLocator = page
@@ -60,7 +49,7 @@ test.describe('Fabric Custom visual regression testing', () => {
 			// compare screenshot to reference
 			await expect(testTemplateLocator).toHaveScreenshot(
 				`Fabric-custom-${breakpoint.replace('%', '')}.png`,
-				{ maxDiffPixelRatio: 0.004 },
+				{ animations: 'disabled', maxDiffPixelRatio: 0.004 },
 			);
 		}
 	});
