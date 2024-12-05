249beb25aac06740cd6dd42e9ec3ed81fcf0998d
Emma Imber <emma-jo.imber@guardian.co.uk>
2024-12-05 12:16:34 +0000

    Skip flaky test

--- playwright/fabric.test.ts
+++ playwright/fabric.test.ts
@@ -3,7 +3,7 @@ import { localBaseUrl, referenceBaseUrl, templatePreviewWidths } from './utils';
 
 const viewport = { width: 1600, height: 1000 };
 
-test.describe('Fabric', () => {
+test.skip('Fabric', () => {
 	test('Get reference screenshots', async ({ page }) => {
 		await page.setViewportSize(viewport);
 
