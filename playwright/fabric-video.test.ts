b978d58bf9f61df39f11a106256edd62b583b4c2
Emma Imber <emma-jo.imber@guardian.co.uk>
2024-12-30 11:48:50 +0000

    Update test name

--- playwright/fabric-video.test.ts
+++ playwright/fabric-video.test.ts
@@ -3,7 +3,7 @@ import { localBaseUrl, referenceBaseUrl, templatePreviewWidths } from './utils';
 
 const viewport = { width: 1600, height: 1000 };
 
-test.describe('Fabric video', () => {
+test.describe('Fabric Video', () => {
 	test('Get reference screenshots', async ({ page }) => {
 		await page.setViewportSize(viewport);
 
