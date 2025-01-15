26bcb0cd6511ef037d9cdd0ebfd65bf4cdf7a5f6
Emma Imber <emma-jo.imber@guardian.co.uk>
2025-01-15 12:41:40 +0000

    Add tests for expanded and not expanded templates

--- playwright/fabric-expandable.test.ts
+++ playwright/fabric-expandable.test.ts
@@ -12,23 +12,30 @@ test.describe('Fabric Expandable', () => {
 		});
 
 		for (const width of templatePreviewWidths) {
-			const referenceTemplateLocator = page
+			const referenceTemplateLocator = page.locator(`[name='width-${width}']`);
+			const referenceTemplateHtmlLocator = page
 				.frameLocator(`[name='width-${width}']`)
 				.locator('html');
 			// check that the template is present on the page
 			expect(referenceTemplateLocator).toBeVisible();
 			// scroll to it
 			await referenceTemplateLocator.scrollIntoViewIfNeeded();
+			// expand the template ad
+			referenceTemplateHtmlLocator.locator('.toggle-arrow').click();
+			// wait for template expansion transition
+			await new Promise((r) => setTimeout(r, 1500));
+			// take a reference screenshot of the expanded template
+			await referenceTemplateLocator.screenshot({
+				path: `./playwright/reference-images/Fabric-expandable-${width.replace('%', '')}-expanded.png`,
+			});
+			// collapse the template ad
+			referenceTemplateHtmlLocator.locator('.toggle-arrow').click();
+			// wait for template collapse transition
+			await new Promise((r) => setTimeout(r, 1500));
 			// take a reference screenshot of the template when not expanded
-			// await referenceTemplateLocator.screenshot({
-			// 	path: `./playwright/reference-images/Fabric-expandable-${width.replace('%', '')}-not-expanded.png`,
-			// });
-			// // expand the template ad
-			// referenceTemplateLocator.locator('.toggle-arrow').click();
-			// // take a reference screenshot of the expanded template
-			// await referenceTemplateLocator.screenshot({
-			// 	path: `./playwright/reference-images/Fabric-expandable-${width.replace('%', '')}-expanded.png`,
-			// });
+			await referenceTemplateLocator.screenshot({
+				path: `./playwright/reference-images/Fabric-expandable-${width.replace('%', '')}-not-expanded.png`,
+			});
 		}
 	});
 
@@ -40,30 +47,32 @@ test.describe('Fabric Expandable', () => {
 		});
 
 		for (const width of templatePreviewWidths) {
-			const testTemplateLocator = page
+			const testTemplateLocator = page.locator(`[name='width-${width}']`);
+			const testTemplateHtmlLocator = page
 				.frameLocator(`[name='width-${width}']`)
 				.locator('html');
 			// check that the template is present on the page
 			expect(testTemplateLocator).toBeVisible();
 			// scroll to it
 			await testTemplateLocator.scrollIntoViewIfNeeded();
-			// compare screenshot to reference
-			// await expect(testTemplateLocator).toHaveScreenshot(
-			// 	`Fabric-expandable-${width.replace('%', '')}.png`,
-			// 	{ maxDiffPixelRatio: 0.006 },
-			// );
-			//take a reference screenshot of the template when not expanded
-			await testTemplateLocator.screenshot({
-				path: `./playwright/reference-images/Fabric-expandable-${width.replace('%', '')}-not-expanded.png`,
-			});
 			// expand the template ad
-			testTemplateLocator.locator('.toggle-arrow').click();
+			testTemplateHtmlLocator.locator('.toggle-arrow').click();
 			// wait for template expansion transition
-			await new Promise((r) => setTimeout(r, 2000));
+			await new Promise((r) => setTimeout(r, 1500));
 			// take a reference screenshot of the expanded template
-			await testTemplateLocator.screenshot({
-				path: `./playwright/reference-images/Fabric-expandable-${width.replace('%', '')}-expanded.png`,
-			});
+			await expect(testTemplateLocator).toHaveScreenshot(
+				`Fabric-expandable-${width.replace('%', '')}-expanded.png`,
+				{ maxDiffPixelRatio: 0.006 },
+			);
+			// collapse the template ad
+			testTemplateHtmlLocator.locator('.toggle-arrow').click();
+			// wait for template collapse transition
+			await new Promise((r) => setTimeout(r, 1500));
+			//take a reference screenshot of the template when not expanded
+			await expect(testTemplateLocator).toHaveScreenshot(
+				`Fabric-expandable-${width.replace('%', '')}-not-expanded.png`,
+				{ maxDiffPixelRatio: 0.006 },
+			);
 		}
 	});
 });
