f878fa3fc1fdf16756dc85e89058d5a13c8ebf71
Emma Imber <emma-jo.imber@guardian.co.uk>
2025-02-28 16:27:38 +0000

    Delete legacy folder

--- legacy/preview/src/js/main.js
+++
@@ -1,9 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-// components
-const App = require('./components/App');
-
-ReactDOM.render(
-	<App />,
-	document.getElementById('content')
-);
--- legacy/src/_shared/js/events-images.js
+++
@@ -1,5 +0,0 @@
-// Generates the HTML for a picture element with responsive sources
-export function generatePicture({ url, classes = [], alt = '' }) {
-    let image = `<img class="${classes.join(' ')}" src="${url}" alt="${alt}">`;
-    return image;
-}
