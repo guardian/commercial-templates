49de5c915fe01e5654c77c16efb34a826d6adb4c
ollie_abrahams <ollie.abrahams@guardian.co.uk>
2026-07-23 10:13:00 +0100

    Revert "Potential fix for pull request finding"
    This reverts commit 7bf6f3a65452864914898b4893589b69bcdfe0f4.

--- scripts/deploy/deploy.ts
+++ scripts/deploy/deploy.ts
@@ -219,9 +219,8 @@ const uploadTemplate = async (
 
 	try {
 		const response = await nativeStyleService.getNativeStylesByStatement(statement);
-		const results = response.results ?? [];
-		if (results.length) {
-			const style = results[0];
+		if (response.results.length) {
+			const style = response.results[0];
 			if (!style) {
 				console.error(
 					`[!] ERROR: No native style found for template "${dirName}" with ${nativeStyleIdField} "${parsedNativeStyleId}". Please check ${nativeStyleIdField} in ad.json.`,
