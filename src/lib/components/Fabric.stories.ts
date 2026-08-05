1c694274e8aa93f03383ed7fc361ce255687884e
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2026-08-05 13:32:21 +0100

    Add Fabric Video and Video XL stories

--- src/lib/components/Fabric.stories.ts
+++ src/lib/components/Fabric.stories.ts
@@ -1,11 +1,33 @@
 import type { Meta, StoryObj } from '@storybook/sveltekit';
 import { gamVariables } from '../../routes/templates/fabric/variables.gam';
+import { gamVariables as fabricVideoGamVariables } from '../../routes/templates/fabric-video/variables.gam';
+import { gamVariables as fabricVideoXLGamVariables } from '../../routes/templates/fabric-video-xl/variables.gam';
 import { gamVariables as fabricXLGamVariables } from '../../routes/templates/fabric-xl/variables.gam';
 import Fabric from './Fabric.svelte';
+
+// stub play() so IntersectionObserver-triggered replays are no-ops
+const preventVideoAutoplay = () => {
+	// eslint-disable-next-line @typescript-eslint/unbound-method -- reassigned as-is, never called detached
+	const originalPlay = window.HTMLMediaElement.prototype.play;
+	window.HTMLMediaElement.prototype.play = () => Promise.resolve();
+
+	return () => {
+		window.HTMLMediaElement.prototype.play = originalPlay;
+	};
+};
+
+// native autoplay can start before the stub above runs, so pause explicitly too
+const pauseVideo = ({ canvasElement }: { canvasElement: HTMLElement }) => {
+	canvasElement.querySelectorAll('video').forEach((video) => {
+		video.removeAttribute('autoplay');
+		video.pause();
+	});
+};
 
 const meta = {
 	title: 'Templates/Fabric',
 	component: Fabric,
+	beforeEach: preventVideoAutoplay,
 	args: {
 		TrackingPixel: gamVariables.Trackingpixel,
 		ResearchPixel: gamVariables.Researchpixel,
@@ -75,4 +97,93 @@ export const XL: Story = {
 			fabricXLGamVariables.MobileLayer3BackgroundPosition,
 		isXL: true,
 	},
+};
+
+export const Video: Story = {
+	args: {
+		TrackingPixel: fabricVideoGamVariables.Trackingpixel,
+		ResearchPixel: fabricVideoGamVariables.Researchpixel,
+		ViewabilityPixel: fabricVideoGamVariables.Viewabilitypixel,
+		BackgroundColour: fabricVideoGamVariables.BackgroundColour,
+		BackgroundImage: fabricVideoGamVariables.BackgroundImage,
+		BackgroundImagePosition: fabricVideoGamVariables.BackgroundPosition,
+		BackgroundImageRepeat: fabricVideoGamVariables.BackgroundRepeat,
+		Layer1BackgroundImage: fabricVideoGamVariables.Layer1BackgroundImage,
+		Layer1BackgroundPosition: fabricVideoGamVariables.Layer1BackgroundPosition,
+		Layer2BackgroundImage: fabricVideoGamVariables.Layer2BackgroundImage,
+		Layer2BackgroundPosition: fabricVideoGamVariables.Layer2BackgroundPosition,
+		Layer3BackgroundImage: fabricVideoGamVariables.Layer3BackgroundImage,
+		Layer3BackgroundPosition: fabricVideoGamVariables.Layer3BackgroundPosition,
+		MobileBackgroundImage: fabricVideoGamVariables.MobileBackgroundImage,
+		MobileBackgroundImagePosition:
+			fabricVideoGamVariables.MobileBackgroundPosition,
+		MobileBackgroundImageRepeat: fabricVideoGamVariables.MobileBackgroundRepeat,
+		MobileLayer1BackgroundImage:
+			fabricVideoGamVariables.MobileLayer1BackgroundImage,
+		MobileLayer1BackgroundPosition:
+			fabricVideoGamVariables.MobileLayer1BackgroundPosition,
+		MobileLayer2BackgroundImage:
+			fabricVideoGamVariables.MobileLayer2BackgroundImage,
+		MobileLayer2BackgroundPosition:
+			fabricVideoGamVariables.MobileLayer2BackgroundPosition,
+		MobileLayer3BackgroundImage:
+			fabricVideoGamVariables.MobileLayer3BackgroundImage,
+		MobileLayer3BackgroundPosition:
+			fabricVideoGamVariables.MobileLayer3BackgroundPosition,
+		VideoURL: fabricVideoGamVariables.VideoURL,
+		VideoURLMobile: fabricVideoGamVariables.VideoURLMobile,
+		VideoBackupImage: fabricVideoGamVariables.VideoBackupImage,
+		MobileVideoBackupImage: fabricVideoGamVariables.MobileVideoBackupImage,
+		VideoAlignment: fabricVideoGamVariables.VideoAlignment,
+		IsFullWidthTopSlot: fabricVideoGamVariables.IsFullWidthTopSlot,
+		showVideo: true,
+	},
+	play: pauseVideo,
+};
+
+export const VideoXL: Story = {
+	args: {
+		TrackingPixel: fabricVideoXLGamVariables.TrackingPixel,
+		ResearchPixel: fabricVideoXLGamVariables.ResearchPixel,
+		ViewabilityPixel: fabricVideoXLGamVariables.ViewabilityPixel,
+		BackgroundColour: fabricVideoXLGamVariables.BackgroundColour,
+		BackgroundImage: fabricVideoXLGamVariables.BackgroundImage,
+		BackgroundImagePosition: fabricVideoXLGamVariables.BackgroundPosition,
+		BackgroundImageRepeat: fabricVideoXLGamVariables.BackgroundRepeat,
+		Layer1BackgroundImage: fabricVideoXLGamVariables.Layer1BackgroundImage,
+		Layer1BackgroundPosition:
+			fabricVideoXLGamVariables.Layer1BackgroundPosition,
+		Layer2BackgroundImage: fabricVideoXLGamVariables.Layer2BackgroundImage,
+		Layer2BackgroundPosition:
+			fabricVideoXLGamVariables.Layer2BackgroundPosition,
+		Layer3BackgroundImage: fabricVideoXLGamVariables.Layer3BackgroundImage,
+		Layer3BackgroundPosition:
+			fabricVideoXLGamVariables.Layer3BackgroundPosition,
+		MobileBackgroundImage: fabricVideoXLGamVariables.MobileBackgroundImage,
+		MobileBackgroundImagePosition:
+			fabricVideoXLGamVariables.MobileBackgroundPosition,
+		MobileBackgroundImageRepeat:
+			fabricVideoXLGamVariables.MobileBackgroundRepeat,
+		MobileLayer1BackgroundImage:
+			fabricVideoXLGamVariables.MobileLayer1BackgroundImage,
+		MobileLayer1BackgroundPosition:
+			fabricVideoXLGamVariables.MobileLayer1BackgroundPosition,
+		MobileLayer2BackgroundImage:
+			fabricVideoXLGamVariables.MobileLayer2BackgroundImage,
+		MobileLayer2BackgroundPosition:
+			fabricVideoXLGamVariables.MobileLayer2BackgroundPosition,
+		MobileLayer3BackgroundImage:
+			fabricVideoXLGamVariables.MobileLayer3BackgroundImage,
+		MobileLayer3BackgroundPosition:
+			fabricVideoXLGamVariables.MobileLayer3BackgroundPosition,
+		VideoURL: fabricVideoXLGamVariables.VideoURL,
+		VideoURLMobile: fabricVideoXLGamVariables.VideoURLMobile,
+		VideoBackupImage: fabricVideoXLGamVariables.VideoBackupImage,
+		MobileVideoBackupImage: fabricVideoXLGamVariables.MobileVideoBackupImage,
+		VideoAlignment: fabricVideoXLGamVariables.VideoAlignment,
+		IsFullWidthTopSlot: 'no',
+		isXL: true,
+		showVideo: true,
+	},
+	play: pauseVideo,
 };
