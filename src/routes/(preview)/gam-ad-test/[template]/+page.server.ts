f62878144168c01a4020c78801d3b69294a04cc1
ollie_abrahams <ollie.abrahams@guardian.co.uk>
2026-08-25 10:31:12 +0100

    simplify page.server.ts

--- src/routes/(preview)/gam-ad-test/[template]/+page.server.ts
+++ src/routes/(preview)/gam-ad-test/[template]/+page.server.ts
@@ -1,6 +1,5 @@
-import { existsSync, readFileSync } from 'fs';
+import { existsSync } from 'fs';
 import { error } from '@sveltejs/kit';
-import { marked } from 'marked';
 import type { PageServerLoad } from './$types';
 import type { RouteId } from '$app/types';
 
@@ -13,7 +12,7 @@ const isValidTemplate = (template: string): template is TemplateName => {
 	return existsSync(`src/routes/templates/${template}`);
 };
 
-export const load = (async ({ params }) => {
+export const load = (({ params }) => {
 	const { template } = params;
 
 	if (!isValidTemplate(template)) {
@@ -21,32 +20,7 @@ export const load = (async ({ params }) => {
 		return error(404, 'Template not found');
 	}
 
-	const srcDir = `src/routes/templates/${template}`;
-
-	const description = existsSync(`${srcDir}/README.md`)
-		? await marked.parse(readFileSync(`${srcDir}/README.md`, 'utf-8'))
-		: `<p><em>no description provided</em></p>`;
-
-	const { gamVariables } = existsSync(`${srcDir}/variables.gam.ts`)
-		? // this import must be relative, so that vite can statically analyze it
-			((await import(`../../../templates/${template}/variables.gam.ts`)) as {
-				gamVariables: Record<string, string | number>;
-			})
-		: { gamVariables: null };
-
-	const adJson = existsSync(`${srcDir}/ad.json`)
-		? (JSON.parse(readFileSync(`${srcDir}/ad.json`, 'utf-8')) as {
-				nativeStyleId: string;
-				creativeTemplateId: string;
-				testNativeStyleId: string;
-				testCreativeId: string;
-			})
-		: null;
-
 	return {
-		description,
 		template,
-		gamVariables,
-		adJson,
 	};
 }) satisfies PageServerLoad;
