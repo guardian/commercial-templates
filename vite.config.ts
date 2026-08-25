c137ddb34fbc8b0e055a807620c37b95ca9f67e5
ollie_abrahams <ollie.abrahams@guardian.co.uk>
2026-08-25 11:18:20 +0100

    add env variable for turning of hmr overlay

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { extractTemplateAssets } from './src/vite/vite-plugin-extract-assets';
 import { transformGAMVariables } from './src/vite/vite-plugin-transform-gam-variables';
 
+const hmrOverlay = process.env.HMR_OVERLAY !== 'false';
+
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [transformGAMVariables(), sveltekit(), extractTemplateAssets()],
@@ -15,7 +17,7 @@ const config = {
 	},
 	server: {
 		hmr: {
-			overlay: false,
+			overlay: hmrOverlay,
 		},
 	},
 };
