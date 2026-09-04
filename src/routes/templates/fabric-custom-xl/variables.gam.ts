5c73ecf5a0f76d70eb031545cf4afcbc08eab489
Charlotte <charlotte.emms@theguardian.com>
2026-09-04 15:57:39 +0100

    rework tracking pixel logic in fabric custom templates

--- src/routes/templates/fabric-custom-xl/variables.gam.ts
+++ src/routes/templates/fabric-custom-xl/variables.gam.ts
@@ -2,7 +2,8 @@ export const gamVariables = {
 	DapAssetsFolder: 'DAP_ad_awards_XL',
 	ThirdPartyTag:
 		'https://tpc.googlesyndication.com/pagead/imgad?id=CICAgKCrtKGjoAEQARgBMgjYagW3ARF6Fw',
-	TrackingPixel: '',
+	TrackingPixel:
+		'https://fakeurl.com/imp/1/328730;11105360;201;pixel;TheGuardian;07285128wwaukdisatvatvawrnsddisgrdncusfcusbcpmaosdskmobtabadevtcxtentenprlpufnnlukenbdcsnsvideo2600x100030sslhnwsstrlsdtprlmnananana/?ft_creative=50126&gdpr=${GDPR}&gdpr_consent=${GDPR_CONSENT_78}&us_privacy=${US_PRIVACY}&oob=0&cachebuster=%%CACHEBUSTER%%',
 	ResearchPixel: '',
 	ViewabilityTracker: `<img src="">`,
 };
--- src/routes/templates/fabric-custom/variables.gam.ts
+++ src/routes/templates/fabric-custom/variables.gam.ts
@@ -1,6 +1,7 @@
 export const gamVariables = {
 	DapAssetsFolder: 'GooglePixel2',
-	TrackingPixel: '',
+	TrackingPixel:
+		'https://fakeurl.com/imp/1/328730;11105360;201;pixel;TheGuardian;07285128wwaukdisatvatvawrnsddisgrdncusfcusbcpmaosdskmobtabadevtcxtentenprlpufnnlukenbdcsnsvideo2600x100030sslhnwsstrlsdtprlmnananana/?ft_creative=50126&gdpr=${GDPR}&gdpr_consent=${GDPR_CONSENT_78}&us_privacy=${US_PRIVACY}&oob=0&cachebuster=%%CACHEBUSTER%%',
 	ResearchPixel: '',
 	ViewabilityTracker: `<img src="">`,
 	ShowLabel: 'yes',
